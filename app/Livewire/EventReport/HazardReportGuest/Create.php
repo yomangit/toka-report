25c642fed13a2d7f6d871d85b37bbead7ed6f15e
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-14 08:01:12 +0800

    sdsd

--- app/Livewire/EventReport/HazardReportGuest/Create.php
+++ app/Livewire/EventReport/HazardReportGuest/Create.php
@@ -351,8 +351,8 @@ class Create extends Component
         $url          = $HazardReport->id;
         $actionUrl = url("/eventReport/hazardReportDetail/{ $url}");
         if ($this->ResponsibleRole == 1) {
-            $moderators = (Auth::check() ? EventUserSecurity::where('responsible_role_id', $this->ResponsibleRole)->where('type_event_report_id', $this->event_type_id)->where('user_id', 'NOT LIKE', Auth::user()->id)->pluck('user_id')->toArray() : EventUserSecurity::where('responsible_role_id', $this->ResponsibleRole)->pluck('user_id')->pluck('user_id')->toArray());
-            $moderators = User::whereIn('id',$moderators)->whereNotNull('email')->get();
+            $moderators_id = (Auth::check() ? EventUserSecurity::where('responsible_role_id', $this->ResponsibleRole)->where('type_event_report_id', $this->event_type_id)->where('user_id', 'NOT LIKE', Auth::user()->id)->pluck('user_id')->toArray() : EventUserSecurity::where('responsible_role_id', $this->ResponsibleRole)->pluck('user_id')->pluck('user_id')->toArray());
+            $moderators = User::whereIn('id',$moderators_id)->whereNotNull('email')->get();
             foreach ($moderators as $moderator) {
                 // Laravel notification
                 $offerData = [
