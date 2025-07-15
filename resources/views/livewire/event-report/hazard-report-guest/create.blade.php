0008cdddbfef5a6ad6f39b703e56cfd09678a5ae
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-15 12:22:02 +0800

    assas

--- resources/views/livewire/event-report/hazard-report-guest/create.blade.php
+++ resources/views/livewire/event-report/hazard-report-guest/create.blade.php
@@ -177,15 +177,11 @@
             <label for="draft" class="text-xs font-semibold peer-checked/draft:text-indigo-500">{{ __('Yes') }}</label>
             <input wire:model.live="show_immidiate" value="no" id="published" class="peer/published checked:bg-sky-500 radio-xs radio" type="radio" name="status" />
             <label for="published" class="text-xs font-semibold peer-checked/published:text-sky-500">{{ __('No') }}</label>
-            <div wire:ignore class="hidden w-full peer-checked/draft:block form-control">
-               <p>a</p>
-            </div>
-            <div wire:ignore class="hidden w-full peer-checked/published:block form-control">
-               <p>b</p>
-            </div>
+            <div class="hidden peer-checked/draft:block">Drafts are only visible to administrators.</div>
+            <div class="hidden peer-checked/published:block">Your post will be publicly visible on your site.</div>
             <x-label-error :messages="$errors->get('immediate_corrective_action')" />
         </fieldset>
-       
+
         <div class="grid grid-rows-3 mt-2 border divide-y-2 md:grid-rows-1 md:grid-cols-3 md:content-center md:gap-4 md:divide-y-0 md:divide-x-2 divide-base-200 border-base-200 rounded-box">
             <div class='px-4 md:place-self-center '>
                 <fieldset class="self-center w-40 max-w-sm fieldset rounded-box">
