6b9f21c70dd386fbb31949a1a0ac03a759e843e5
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-15 15:39:52 +0800

    fddfd

--- resources/views/livewire/event-report/hazard-report/action/index.blade.php
+++ resources/views/livewire/event-report/hazard-report/action/index.blade.php
@@ -1,11 +1,11 @@
 <div>
     <div class="flex justify-start gap-2 mb-2 md:justify-between">
-            <x-btn-add data-tip="Add"  wire:click="$dispatch('modalActionHazard', @js($reference), @js($hazard_id))"  class="{{ $current_step === 'Closed' || $current_step === 'Cancelled' ? 'btn-disabled' : '' }}" />
+            <x-btn-add data-tip="Add"  wire:click="$dispatch('modalActionHazardNew',{ reference:{{ $reference }},hazard: {{ $hazard_id }} })"  class="{{ $current_step === 'Closed' || $current_step === 'Cancelled' ? 'btn-disabled' : '' }}" />
            <div class="w-full max-w-xs" > <x-inputsearch wire:model.live='search' /></div>
     </div>
     @forelse ($ActionHazard as $no => $index)
         <div class="flex flex-col items-center justify-between p-2 mb-2 border-b-2 sm:flex-row border-base-300">
-            <div class="w-full  basis-1/5">
+            <div class="w-full basis-1/5">
                 <div class="space-x-4 text-gray-500">
                     <span class="font-mono text-[10px] font-semibold">Action No.</span>
                     <span class="text-[10px] font-semibold">{{ $ActionHazard->firstItem() + $no }} </span>
@@ -16,7 +16,7 @@
                     </div>
                 </div>
             </div>
-            <div class="w-full p-4  basis-3/4">
+            <div class="w-full p-4 basis-3/4">
                 <div class="flex flex-col items-stretch divide-y divide-gray-400 ">
 
                     <div class="grid flex-row grid-cols-4 gap-1 text-gray-500">
@@ -41,7 +41,7 @@
                     </div>
                 </div>
             </div>
-            <div class="w-full  basis-2/3">
+            <div class="w-full basis-2/3">
                 <div class="flex flex-col items-stretch divide-y divide-gray-400 ">
                     <div class="flex-row">
                         <div class="grid grid-cols-3 text-gray-500">
