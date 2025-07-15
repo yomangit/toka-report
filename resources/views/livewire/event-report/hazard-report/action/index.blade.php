02eb371c1e22a050b9c9a8915647730e7f12124f
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-15 15:23:17 +0800

    dsdsd

--- resources/views/livewire/event-report/hazard-report/action/index.blade.php
+++ resources/views/livewire/event-report/hazard-report/action/index.blade.php
@@ -1,12 +1,13 @@
 <div>
-    <div class="flex justify-start md:justify-between mb-2 gap-2">
-            <x-btn-add data-tip="Add"  wire:click="$dispatch('modalActionHazard',{ hazard: {{ $hazard_id }} })"  class="{{ $current_step === 'Closed' || $current_step === 'Cancelled' ? 'btn-disabled' : '' }}" />
+    <div class="flex justify-start gap-2 mb-2 md:justify-between">
+            <x-btn-add data-tip="Add"  wire:click="$dispatch('modalActionHazard', { reference: @js($reference),hazard: @js($hazard_id)  })"
+  class="{{ $current_step === 'Closed' || $current_step === 'Cancelled' ? 'btn-disabled' : '' }}" />
            <div class="w-full max-w-xs" > <x-inputsearch wire:model.live='search' /></div>
     </div>
     @forelse ($ActionHazard as $no => $index)
-        <div class="flex flex-col sm:flex-row items-center justify-between border-b-2 mb-2 border-base-300 p-2">
-            <div class=" w-full basis-1/5">
-                <div class="text-gray-500 space-x-4">
+        <div class="flex flex-col items-center justify-between p-2 mb-2 border-b-2 sm:flex-row border-base-300">
+            <div class="w-full  basis-1/5">
+                <div class="space-x-4 text-gray-500">
                     <span class="font-mono text-[10px] font-semibold">Action No.</span>
                     <span class="text-[10px] font-semibold">{{ $ActionHazard->firstItem() + $no }} </span>
                     <div class="flex gap-2">
@@ -16,55 +17,55 @@
                     </div>
                 </div>
             </div>
-            <div class=" w-full basis-3/4 p-4">
+            <div class="w-full p-4  basis-3/4">
                 <div class="flex flex-col items-stretch divide-y divide-gray-400 ">
 
-                    <div class="flex-row text-gray-500 grid grid-cols-4 gap-1">
+                    <div class="grid flex-row grid-cols-4 gap-1 text-gray-500">
                         <span class="font-mono text-[10px] font-semibold ">Hazard</span>
                         <span
                             class="font-mono text-[10px] text-justify font-semibold col-span-3">{{ $task_being_done }}</span>
                     </div>
-                    <div class="flex-row text-gray-500 grid grid-cols-4 gap-1 ">
+                    <div class="grid flex-row grid-cols-4 gap-1 text-gray-500 ">
                         <span class="font-mono text-[10px] font-semibold ">Followup Action</span>
                         <span
                             class="font-mono text-[10px] text-justify font-semibold col-span-3">{{ $index->followup_action }}</span>
                     </div>
-                    <div class="flex-row text-gray-500 grid grid-cols-4 gap-1 ">
+                    <div class="grid flex-row grid-cols-4 gap-1 text-gray-500 ">
                         <span class="font-mono text-[10px] font-semibold ">Actionee Comments</span>
                         <span class="font-mono text-[10px] text-justify font-semibold col-span-3">
                             {{ $index->actionee_comment }}</span>
                     </div>
-                    <div class="flex-row text-gray-500 grid grid-cols-4 gap-1 ">
+                    <div class="grid flex-row grid-cols-4 gap-1 text-gray-500 ">
                         <span class="font-mono text-[10px] font-semibold ">Action Conditions</span>
                         <span class="font-mono text-[10px] font-semibold col-span-3">
                             {{ $index->action_condition }}</span>
                     </div>
                 </div>
             </div>
-            <div class=" w-full basis-2/3 ">
+            <div class="w-full  basis-2/3">
                 <div class="flex flex-col items-stretch divide-y divide-gray-400 ">
                     <div class="flex-row">
-                        <div class="text-gray-500 grid grid-cols-3">
+                        <div class="grid grid-cols-3 text-gray-500">
                             <span class="font-mono text-[10px] font-semibold ">{{ __('Orginal Due Date') }}</span>
                             <span class="font-mono text-[10px] font-semibold "> {{ $orginal_due_date }}</span>
                         </div>
                     </div>
                     <div class="flex-row">
-                        <div class="text-gray-500 grid grid-cols-3">
+                        <div class="grid grid-cols-3 text-gray-500">
                             <span class="font-mono text-[10px] font-semibold ">{{ __('Responsibility') }}</span>
                             <span class="font-mono text-[10px] col-span-2 font-semibold ">
                                 {{ $index->users->lookup_name }}</span>
                         </div>
                     </div>
                     <div class="flex-row">
-                        <div class="text-gray-500 grid grid-cols-3">
+                        <div class="grid grid-cols-3 text-gray-500">
                             <span class="font-mono text-[10px] font-semibold ">{{ __('Due Date') }}</span>
                             <span class="font-mono text-[10px] col-span-2 font-semibold ">
                                 {{ $index->due_date }}</span>
                         </div>
                     </div>
                     <div class="flex-row">
-                        <div class="text-gray-500 grid grid-cols-3">
+                        <div class="grid grid-cols-3 text-gray-500">
                             <span class="font-mono text-[10px] font-semibold ">{{ __('Completion Date') }}</span>
                             <span class="font-mono text-[10px] col-span-2 font-semibold ">
                                 {{ $index->completion_date }}</span>
@@ -74,7 +75,7 @@
             </div>
         </div>
         @empty
-        <div class="text-rose-500 font-semibold text-center text-xs">there is no action</div>
+        <div class="text-xs font-semibold text-center text-rose-500">there is no action</div>
     @endforelse
 
 </div>
