2a2e3c40652e72fda853529773f1b72e5c4b4e7c
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-17 23:30:17 +0800

    df

--- resources/views/livewire/admin/manage-division-access.blade.php
+++ resources/views/livewire/admin/manage-division-access.blade.php
@@ -4,7 +4,7 @@
     <x-btn-add data-tip="Tambah Data" wire:click="openCreateModal" />
     <div class="p-4">
         <div class="relative w-full" wire:click.away="$set('showUserDropdown', false)">
-            <input type="text" wire:model.live="searchUserQuery" class="w-full input input-bordered input-sm" placeholder="Cari nama user...">
+            <x-inputsearch wire:model.live="searchUserQuery" placeholder="Cari nama user...">
 
             @if ($showUserDropdown && strlen($searchUserQuery) > 1)
             <ul class="absolute z-10 w-full mt-1 overflow-auto text-sm bg-white border border-gray-300 rounded shadow max-h-60">
