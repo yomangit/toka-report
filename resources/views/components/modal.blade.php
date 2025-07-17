059ab235dbffed7edea361de948a31aff0aa15bd
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-17 14:56:21 +0800

    dfas

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,5 +1,5 @@
 <div class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40" x-data="{ open: @entangle($attributes->wire('model')) }" x-show="open">
-    <div @click.away="open = false" class="w-full max-w-lg p-6 bg-white rounded shadow-lg">
+    <div @click.away="open = false" class="w-full max-w-lg p-6 rounded shadow-lg">
         <div class="text-lg font-semibold">
             {{ $title }}
         </div>
