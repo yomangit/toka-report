7731df6e239bd37964f87db1a00266f63cbe70a4
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-18 07:52:50 +0800

    dsd

--- app/Livewire/Admin/ManageDivisionAccess.php
+++ app/Livewire/Admin/ManageDivisionAccess.php
@@ -106,7 +106,7 @@ class ManageDivisionAccess extends Component
     {
         return view('livewire.admin.manage-division-access', [
             'users' => User::searchNama($this->search_nama)->paginate(20),
-            'user_select' => User::get(),
+            'user_select' => User::take(20)->get(),
             'divisions' => Division::all()
         ])->extends('base.index', ['header' => 'Akeses Divisi', 'title' => 'Akeses Divisi'])->section('content');
     }
