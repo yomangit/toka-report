139e754ed9ad8448d09bc2091e3aedbf5fe8a002
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-17 19:51:57 +0800

    sdsdf

--- resources/js/tom-search.js
+++ resources/js/tom-search.js
@@ -1,24 +1,9 @@
 import TomSelect from 'tom-select';
 
-function initTomSelect() {
-    const selectEl = document.getElementById('user-select');
-    if (selectEl && !selectEl.tomselect) {
-        new TomSelect(selectEl, {
-            create: true,
-            sortField: {
-                field: "text",
-                direction: "asc"
-            }
-        });
-    }
-}
-
-// Inisialisasi saat pertama kali load
-document.addEventListener('livewire:load', () => {
-    initTomSelect();
-});
-
-// Re-init setelah Livewire DOM update (misalnya habis submit, buka modal, dsb)
-Livewire.hook('message.processed', () => {
-    initTomSelect();
+new TomSelect("#user-select",{
+	create: true,
+	sortField: {
+		field: "text",
+		direction: "asc"
+	}
 });
