f9b7936e8bf0674549875a9b3f03c60042f780e8
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-18 10:49:21 +0800

    sd

--- resources/views/components/inputsearch.blade.php
+++ resources/views/components/inputsearch.blade.php
@@ -1,7 +1,7 @@
 @props(['value', 'name','placeholder'])
 
 
-<label class="relative block w-full sm:max-w-lg">
+<label class="relative block w-full sm:max-w-xs">
     <span class="absolute inset-y-0 left-0 flex items-center pl-2">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
             <path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" />
