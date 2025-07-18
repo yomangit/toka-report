07a16ed52ed505827bff963ddc4f4d9bb3028c9d
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-18 08:36:32 +0800

    asas

--- resources/views/livewire/admin/event-category/index.blade.php
+++ resources/views/livewire/admin/event-category/index.blade.php
@@ -1,52 +1,64 @@
-<div class="p-4">
+<div class="p-2 sm:p-4">
     <x-notification />
 
-    {{-- Header Action --}}
+    {{-- Tombol & Pencarian Responsif --}}
     <div class="flex flex-col gap-2 mb-4 sm:flex-row sm:items-center sm:justify-between">
-        <x-btn-add 
-            class="w-full sm:w-auto" 
-            data-tip="Add data" 
-            wire:click="$dispatch('openModal', { component: 'admin.event-category.create-and-update' })" 
-        />
-
-        <x-inputsearch 
-            name="search" 
-            wire:model.live="search" 
-            class="w-full sm:w-64"
-        />
+        <div>
+            <x-btn-add 
+                class="w-full sm:w-auto"
+                data-tip="Add data" 
+                wire:click="$dispatch('openModal', { component: 'admin.event-category.create-and-update' })" 
+            />
+        </div>
+        <div class="w-full sm:w-64">
+            <x-inputsearch 
+                name='search' 
+                wire:model.live='search'
+                class="w-full"
+            />
+        </div>
     </div>
 
-    {{-- Grid Card Style --}}
-    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        @forelse ($EventCategory as $no => $cc)
-            <div class="p-4 bg-white rounded-lg shadow">
-                <div class="flex items-center justify-between mb-2">
-                    <span class="font-bold text-gray-700">#{{ $EventCategory->firstItem() + $no }}</span>
-                    <div class="flex gap-1">
-                        <x-icon-btn-edit 
-                            data-tip="Edit" 
-                            wire:click="$dispatch('openModal', { component: 'admin.event-category.create-and-update', arguments: { event_category: {{ $cc->id }} }})" 
-                        />
-                        <x-icon-btn-delete 
-                            wire:click="delete({{ $cc->id }})" 
-                            wire:confirm.prompt="Are you sure delete?\n\nType DELETE to confirm|DELETE" 
-                            data-tip="Delete" 
-                        />
-                    </div>
-                </div>
-                <div class="text-gray-800">
-                    <p class="font-semibold">{{ $cc->event_category_name }}</p>
-                </div>
-            </div>
-        @empty
-            <div class="text-center col-span-full text-error">
-                Data not found!!!
-            </div>
-        @endforelse
-    </div>
+    {{-- Table --}}
+    <div class="overflow-x-auto bg-white rounded shadow">
+        <table class="table w-full text-sm table-zebra table-xs">
+            <thead>
+                <tr class="text-center text-gray-700 bg-gray-100">
+                    <th class="px-2 py-3">#</th>
+                    <th class="px-2 py-3">Name</th>
+                    <th class="px-2 py-3">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($EventCategory as $no => $cc)
+                <tr class="text-center">
+                    <th class="px-2 py-2">{{ $EventCategory->firstItem() + $no }}</th>
+                    <td class="px-2 py-2">{{ $cc->event_category_name }}</td>
+                    <td class="px-2 py-2">
+                        <div class="flex flex-wrap justify-center gap-1">
+                            <x-icon-btn-edit 
+                                data-tip="Edit" 
+                                wire:click="$dispatch('openModal', { component: 'admin.event-category.create-and-update', arguments: { event_category: {{ $cc->id }} }})" 
+                            />
+                            <x-icon-btn-delete 
+                                wire:click="delete({{ $cc->id }})" 
+                                wire:confirm.prompt="Are you sure delete  ?\n\nType DELETE to confirm|DELETE" 
+                                data-tip="Delete" 
+                            />
+                        </div>
+                    </td>
+                </tr>
+                @empty
+                <tr class="text-center">
+                    <td colspan="3" class="py-4 text-error">Data not found!!!</td>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
 
-    {{-- Pagination --}}
-    <div class="mt-6">
-        {{ $EventCategory->links() }}
+        {{-- Pagination --}}
+        <div class="mt-4">
+            {{ $EventCategory->links() }}
+        </div>
     </div>
 </div>
