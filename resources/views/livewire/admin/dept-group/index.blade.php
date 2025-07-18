840b942a1a07cea064065e12ba5dbf4d4c253130
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-18 09:59:28 +0800

    asdf

--- resources/views/livewire/admin/dept-group/index.blade.php
+++ resources/views/livewire/admin/dept-group/index.blade.php
@@ -1,9 +1,9 @@
 <div>
     <x-notification />
-    <div>
-        <div role="tablist" class="tabs tabs-lifted">
-            <input type="radio" name="my_tabs_2" role="tab" class="font-semibold tab" aria-label="Department Group" checked />
-            <div role="tabpanel" class="p-6 tab-content bg-base-100 border-base-300 rounded-box">
+    <div role="tablist" class="tabs tabs-lifted">
+        <input type="radio" name="my_tabs_2" role="tab" class="font-semibold tab" aria-label="Department Group" checked />
+        <div role="tabpanel" class="p-6 tab-content bg-base-100 border-base-300 rounded-box">
+            <div>
                 <div class="flex flex-col sm:flex-row sm:justify-between ">
                     <div>
                         <x-btn-add data-tip="Add data" wire:click="$dispatch('openModal', { component: 'admin.dept-group.create-and-update' })" />
@@ -23,58 +23,58 @@
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div class="overflow-x-auto">
-                    <table class="table table-zebra table-xs">
-                        <!-- head -->
-                        <thead>
-                            <tr class="text-center">
-                                <th>#</th>
-                                <th class="font-extrabold">Group</th>
-                                <th class="font-extrabold">Department</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <!-- row 1 -->
-                            @forelse ($Group as $no => $group)
-                            <tr>
-                                <th class="text-center">{{ $Group->firstItem() + $no }}</th>
-                                <td class="font-bold text-center">{{ $group->group_name }}</td>
-                                <td>
+            <div class="overflow-x-auto">
+                <table class="table table-zebra table-xs">
+                    <!-- head -->
+                    <thead>
+                        <tr class="text-center">
+                            <th>#</th>
+                            <th class="font-extrabold">Group</th>
+                            <th class="font-extrabold">Department</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- row 1 -->
+                        @forelse ($Group as $no => $group)
+                        <tr>
+                            <th class="text-center">{{ $Group->firstItem() + $no }}</th>
+                            <td class="font-bold text-center">{{ $group->group_name }}</td>
+                            <td>
 
-                                    @foreach ($group->Dept()->get() as $dept)
-                                    <div class="grid items-center grid-cols-2 hover:bg-slate-300">
+                                @foreach ($group->Dept()->get() as $dept)
+                                <div class="grid items-center grid-cols-2 hover:bg-slate-300">
 
-                                        <div class="m-1 font-semibold text-right ">
-                                            {{ $dept->department_name }}
-                                        </div>
-                                        <div class="m-1">
-
-                                            <x-icon-btn-edit data-tip="Edit" wire:click="$dispatch('openModal', { component: 'admin.dept-group.create-and-update', arguments: { group: {{ $group->id }} , dept:{{ $dept->id }} }})" />
-                                            <x-icon-btn-delete wire:click="delete({{ $group->id }}, {{ $dept->id }})" wire:confirm.prompt="Are you sure delete {{ $group->name_company }}?\n\nType DELETE to confirm|DELETE" data-tip="Delete" />
-                                        </div>
+                                    <div class="m-1 font-semibold text-right ">
+                                        {{ $dept->department_name }}
                                     </div>
-                                    @endforeach
-                                </td>
+                                    <div class="m-1">
 
-                            </tr>
-                            @empty
-                            <tr class="text-center">
-                                <th colspan="4" class="text-error">data not found!!! </th>
-                            </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
-                    <div>{{ $Group->links() }}</div>
-                </div>
+                                        <x-icon-btn-edit data-tip="Edit" wire:click="$dispatch('openModal', { component: 'admin.dept-group.create-and-update', arguments: { group: {{ $group->id }} , dept:{{ $dept->id }} }})" />
+                                        <x-icon-btn-delete wire:click="delete({{ $group->id }}, {{ $dept->id }})" wire:confirm.prompt="Are you sure delete {{ $group->name_company }}?\n\nType DELETE to confirm|DELETE" data-tip="Delete" />
+                                    </div>
+                                </div>
+                                @endforeach
+                            </td>
+
+                        </tr>
+                        @empty
+                        <tr class="text-center">
+                            <th colspan="4" class="text-error">data not found!!! </th>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+                <div>{{ $Group->links() }}</div>
             </div>
-
-            <input type="radio" name="my_tabs_2" role="tab" class="font-semibold tab" aria-label="Group" />
-            <div role="tabpanel" class="p-6 tab-content bg-base-100 border-base-300 rounded-box">
-                @livewire('admin.group.index')
-            </div>
-
-
         </div>
+
+        <input type="radio" name="my_tabs_2" role="tab" class="font-semibold tab" aria-label="Group" />
+        <div role="tabpanel" class="p-6 tab-content bg-base-100 border-base-300 rounded-box">
+            @livewire('admin.group.index')
+        </div>
+
+
     </div>
 </div>
--- resources/views/livewire/admin/group/index.blade.php
+++ resources/views/livewire/admin/group/index.blade.php
@@ -1,8 +1,12 @@
 <div>
-    <div class="flex flex-col sm:flex-row sm:justify-between ">
-        <div><x-btn-add data-tip="Add data" wire:click="$dispatch('openModal', { component: 'admin.group.create-and-update' })" /></div>
-        <div>
-            <x-inputsearch name='search' wire:model.live='search' />
+    <div>
+        <div class="flex flex-col sm:flex-row sm:justify-between ">
+            <div>
+                <x-btn-add data-tip="Add data" wire:click="$dispatch('openModal', { component: 'admin.group.create-and-update' })" />
+            </div>
+            <div>
+                <x-inputsearch name='search' wire:model.live='search' />
+            </div>
         </div>
     </div>
 
@@ -19,24 +23,21 @@
             <tbody>
                 <!-- row 1 -->
                 @forelse ($Group as $no => $groups)
-                    <tr class="text-center">
-                        <th>{{ $Group->firstItem() + $no }}</th>
-                        <td>{{ $groups->group_name }}</td>
-                        <td>
-                            <div class="flex flex-row gap-1 justify-center">
-                                <x-icon-btn-edit data-tip="Edit"
-                                    wire:click="$dispatch('openModal', { component: 'admin.group.create-and-update', arguments: { group: {{ $groups->id }}  }})" />
-                                <x-icon-btn-delete wire:click="delete({{ $groups->id }})"
-                                    wire:confirm.prompt="Are you sure delete {{$groups->group_name }}?\n\nType DELETE to confirm|DELETE"
-                                    data-tip="Delete" />
+                <tr class="text-center">
+                    <th>{{ $Group->firstItem() + $no }}</th>
+                    <td>{{ $groups->group_name }}</td>
+                    <td>
+                        <div class="flex flex-row justify-center gap-1">
+                            <x-icon-btn-edit data-tip="Edit" wire:click="$dispatch('openModal', { component: 'admin.group.create-and-update', arguments: { group: {{ $groups->id }}  }})" />
+                            <x-icon-btn-delete wire:click="delete({{ $groups->id }})" wire:confirm.prompt="Are you sure delete {{$groups->group_name }}?\n\nType DELETE to confirm|DELETE" data-tip="Delete" />
 
-                            </div>
-                        </td>
-                    </tr>
+                        </div>
+                    </td>
+                </tr>
                 @empty
-                    <tr class="text-center">
-                        <th colspan="3" class="text-error">data not found!!! </th>
-                    </tr>
+                <tr class="text-center">
+                    <th colspan="3" class="text-error">data not found!!! </th>
+                </tr>
                 @endforelse
             </tbody>
         </table>
