c14005736dccdf84734792b43336ae60e776eb18
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-18 10:48:46 +0800

    as

--- resources/views/components/inputsearch.blade.php
+++ resources/views/components/inputsearch.blade.php
@@ -9,7 +9,7 @@
 
     </span>
     <input {{ $attributes->class([
-            'input input-bordered input-xs font-sans font-semibold placeholder:italic placeholder:text-slate-400  border border-slate-300 pl-6 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-xs',
+            'input input-bordered input-xs font-sans font-semibold placeholder:italic placeholder:text-slate-400 w-full border border-slate-300 pl-6 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-xs',
             
         ]) }} type="text" name="search" @isset($name) name="{{ $name }}" @endif @isset($placeholder) placeholder="{{ $placeholder }}" @endif type="text" @isset($value) value="{{ $value }}" @endif {{ $attributes }} autocomplete="off" />
 </label>
