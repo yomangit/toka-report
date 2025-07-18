448c300ebe0e93970cfa98999804929006447570
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-18 09:38:17 +0800

    aa

--- resources/views/livewire/admin/company/index.blade.php
+++ resources/views/livewire/admin/company/index.blade.php
@@ -1,25 +1,26 @@
 <div>
     <x-notification />
-    <div class="flex flex-col sm:flex-row sm:justify-between ">
-        <div>
-            <x-btn-add data-tip="Add data" wire:click="$dispatch('openModal', { component: 'admin.company.create-and-update' })" />
-            <x-btn-upload data-tip="Upload File" wire:click="$dispatch('openModal', { component: 'admin.company.upload' })" />
-        </div>
-        <div>
-            <div class="flex flex-col sm:flex-row gap-1">
-                <x-inputsearch name='search' wire:model.live='search' />
-                <x-select-search wire:model.live='search_companty_category'>
-                    <option class="opacity-40" value="" selected>Select All</option>
-                    @foreach ($CompanyCategory as $company_category)
+    <div>
+        <div class="flex flex-col sm:flex-row sm:justify-between ">
+            <div>
+                <x-btn-add data-tip="Add data" wire:click="$dispatch('openModal', { component: 'admin.company.create-and-update' })" />
+                <x-btn-upload data-tip="Upload File" wire:click="$dispatch('openModal', { component: 'admin.company.upload' })" />
+            </div>
+            <div>
+                <div class="flex flex-col gap-1 sm:flex-row">
+                    <x-inputsearch name='search' wire:model.live='search' />
+                    <x-select-search wire:model.live='search_companty_category'>
+                        <option class="opacity-40" value="" selected>Select All</option>
+                        @foreach ($CompanyCategory as $company_category)
                         <option value="{{ $company_category->name_category_company }}">
                             {{ $company_category->name_category_company }}
                         </option>
-                    @endforeach
-                </x-select-search>
+                        @endforeach
+                    </x-select-search>
+                </div>
             </div>
         </div>
     </div>
-
     <div class="overflow-x-auto">
         <table class="table table-zebra table-xs">
             <!-- head -->
@@ -34,22 +35,19 @@
             <tbody>
                 <!-- row 1 -->
                 @forelse ($Company as $no => $cc)
-                    <tr class="text-center">
-                        <th>{{ $Company->firstItem() + $no }}</th>
-                        <td>{{ $cc->CompanyCategory->name_category_company }}</td>
-                        <td>{{ $cc->name_company }}</td>
-                        <td class="flex flex-row gap-1 justify-center">
-                            <x-icon-btn-edit data-tip="Edit"
-                                wire:click="$dispatch('openModal', { component: 'admin.company.create-and-update', arguments: { company: {{ $cc->id }} }})" />
-                            <x-icon-btn-delete wire:click="delete({{ $cc->id }})"
-                                wire:confirm.prompt="Are you sure delete {{ $cc->name_company }}?\n\nType DELETE to confirm|DELETE"
-                                data-tip="Delete" />
-                        </td>
-                    </tr>
+                <tr class="text-center">
+                    <th>{{ $Company->firstItem() + $no }}</th>
+                    <td>{{ $cc->CompanyCategory->name_category_company }}</td>
+                    <td>{{ $cc->name_company }}</td>
+                    <td class="flex flex-row justify-center gap-1">
+                        <x-icon-btn-edit data-tip="Edit" wire:click="$dispatch('openModal', { component: 'admin.company.create-and-update', arguments: { company: {{ $cc->id }} }})" />
+                        <x-icon-btn-delete wire:click="delete({{ $cc->id }})" wire:confirm.prompt="Are you sure delete {{ $cc->name_company }}?\n\nType DELETE to confirm|DELETE" data-tip="Delete" />
+                    </td>
+                </tr>
                 @empty
-                    <tr class="text-center">
-                        <th colspan="4" class="text-error">data not found!!! </th>
-                    </tr>
+                <tr class="text-center">
+                    <th colspan="4" class="text-error">data not found!!! </th>
+                </tr>
                 @endforelse
             </tbody>
         </table>
