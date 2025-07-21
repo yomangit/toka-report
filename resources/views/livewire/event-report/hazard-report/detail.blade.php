b4a26ecb26347d613bf07da7885ce3aef881345d
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-21 12:01:16 +0800

    A

--- resources/views/livewire/event-report/hazard-report/detail.blade.php
+++ resources/views/livewire/event-report/hazard-report/detail.blade.php
@@ -305,9 +305,10 @@
                                             {{ $risk_likelihood->risk_likelihoods_name }}
                                         </th>
                                         @foreach ($risk_likelihood->RiskAssessment()->get() as $risk_assessment)
+                                        @php $data = App\Models\TableRiskAssessment::where('risk_likelihood_id', $risk_likelihood->id)->where('risk_consequence_id', $risk_consequence->id)->first()->risk_assessment_id ; @endphp
                                         <th wire:click="riskId({{ $risk_likelihood->id }},
 											{{ $risk_consequence->id }},
-											{{ App\Models\TableRiskAssessment::where('risk_likelihood_id', $risk_likelihood->id)->where('risk_consequence_id', $risk_consequence->id)->first()->risk_assessment_id }})
+											{{$data}})
 											" class=" p-0 text-xs font-semibold text-center border-2 border-black {{ $currentStep === 'Closed' || $currentStep === 'Cancelled' ? ' opacity-35 bg-gray-500' :  $risk_assessment->colour }}">
 
                                         </th>
