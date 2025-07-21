b944e44dd8be5881a7c1054d7109a8082b002e8e
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-21 15:23:38 +0800

    ssas

--- resources/views/livewire/event-report/hazard-report/detail.blade.php
+++ resources/views/livewire/event-report/hazard-report/detail.blade.php
@@ -305,15 +305,10 @@
                                             {{ $risk_likelihood->risk_likelihoods_name }}
                                         </th>
                                         @foreach ($risk_likelihood->RiskAssessment()->get() as $risk_assessment)
-                                        @php
-                                        $consequence = $TableRisk->where('risk_likelihood_id', $risk_likelihood->id)->where('risk_assessment_id', $risk_assessment->id)->first()->risk_consequence_id;
-                                        $id_matrix = $TableRisk->where('risk_likelihood_id', $risk_likelihood->id)->where('risk_assessment_id', $risk_assessment->id)->first()->id;
-
-                                        @endphp
                                         <th wire:click="riskId({{ $risk_likelihood->id }},
 											{{ $risk_assessment->id }},
-											{{ $consequence }})
-											" class=" p-0 text-xs font-semibold text-center border-2 border-black cursor-pointer {{ $tablerisk_id === $id_matrix ? 'border-4 border-neutral scale-105 ring ring-offset-1' : '' }} {{ $risk_assessment->colour }}">
+											{{ $TableRisk->where('risk_likelihood_id', $risk_likelihood->id)->where('risk_assessment_id', $risk_assessment->id)->first()->risk_consequence_id }})
+											" class=" p-0 text-xs font-semibold text-center border-2 border-black cursor-pointer {{ $risk_assessment->colour }}">
                                             {{-- {{ $risk_assessment->risk_assessments_name }} --}}
                                         </th>
                                         @endforeach
