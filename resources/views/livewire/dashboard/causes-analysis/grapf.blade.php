0c29cfc1d5f5fa816aeb5f31fd6e443a49108fbc
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-22 15:12:40 +0800

    sds

--- resources/views/livewire/dashboard/causes-analysis/grapf.blade.php
+++ resources/views/livewire/dashboard/causes-analysis/grapf.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <div id="perbandinganPieChart" data-labels='@json($labels)' data-counts='@json($counts)' class="w-full w-80 h-96"></div>
+    <div id="perbandinganPieChart" data-labels='@json($labels)' data-counts='@json($counts)' class=" w-80 h-96"></div>
 </div>
