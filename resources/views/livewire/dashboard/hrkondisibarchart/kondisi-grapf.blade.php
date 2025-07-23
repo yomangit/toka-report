6658882bd814252ed65f63126fd946ccfdfae51e
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-23 14:03:22 +0800

    ccc

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -1,8 +1,27 @@
 <div>
-    <div
-    id="kondisiBarChart"
-    data-labels='@json($labels)'
-    data-counts='@json($counts)'
-    class="w-full h-96"
-></div>
+    <div id="kondisiBarChart"></div>
+
+    <script type="module">
+        import ApexCharts from 'apexcharts';
+
+        const labels = @json($labels);
+        const counts = @json($counts);
+
+        const options = {
+            chart: {
+                type: 'bar',
+                height: 350
+            },
+            series: [{
+                name: 'Jumlah Laporan',
+                data: counts
+            }],
+            xaxis: {
+                categories: labels
+            }
+        };
+
+        const chart = new ApexCharts(document.querySelector("#kondisiBarChart"), options);
+        chart.render();
+    </script>
 </div>
