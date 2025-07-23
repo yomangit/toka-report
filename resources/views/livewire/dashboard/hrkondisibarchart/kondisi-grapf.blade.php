1c0032fe313eee8287b3b278d824c1aa2fa7b607
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-23 14:11:58 +0800

    dsd

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div id="kondisiBarChart"></div>
 
-    <script type="module">
+    <script>
         import ApexCharts from 'apexcharts';
 
         const labels = @json($labels);
@@ -9,19 +9,44 @@
 
         const options = {
             chart: {
-                type: 'bar',
-                height: 350
-            },
-            series: [{
-                name: 'Jumlah Laporan',
-                data: counts
-            }],
-            xaxis: {
-                categories: labels
+                type: 'bar'
+                , height: 350
             }
+            , series: [{
+                name: 'Jumlah Laporan'
+                , data: counts
+            }],
+
+            title: {
+                text: 'Laporan per Kondisi Tidak Aman'
+                , align: 'center'
+                , style: {
+                    fontSize: '10px'
+                }
+            }
+            , xaxis: {
+                categories: labels
+                , labels: {
+                    style: {
+                        fontSize: '10px'
+                    }
+                    , formatter: function(value) {
+                        return value.length > 15 ? value.substring(0, 12) + '…' : value;
+                    }
+                , }
+            , }
+            , plotOptions: {
+                bar: {
+                    horizontal: false
+                    , borderRadius: 4
+                    , distributed: true
+                }
+            }
+            , colors: ['#3f51b5', '#00bcd4', '#8bc34a', '#ff9800', '#e91e63']
         };
 
         const chart = new ApexCharts(document.querySelector("#kondisiBarChart"), options);
         chart.render();
+
     </script>
 </div>
