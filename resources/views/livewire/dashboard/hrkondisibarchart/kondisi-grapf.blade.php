dbeed0453b96e7dbfae4755541123a21693739c2
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-24 07:57:58 +0800

    sdsd

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -1,26 +1,30 @@
-@vite(['resources/js/charts/kondisiChart.js'])
-<div wire:init="loadChartData">
-    <div id="kondisiChart" wire:ignore></div>
+<div>
+    @vite(['resources/js/charts/kondisiChart.js'])
+    <div wire:init="loadChartData">
+        <div id="kondisiChart" wire:ignore></div>
+    </div>
+    <script>
+        // Passing PHP Livewire data ke JS
+        const labels = @json($labels);
+        const counts = @json($counts);
+
+        console.log('Labels:', labels);
+        console.log('Counts:', counts);
+
+        const chartOptions = {
+            chart: {
+                type: 'bar'
+                , height: 350
+            }
+            , series: [{
+                name: 'Jumlah'
+                , data: counts
+            }]
+            , xaxis: {
+                categories: labels
+            }
+        };
+
+    </script>
+
 </div>
-<script>
-    // Passing PHP Livewire data ke JS
-    const labels = @json($labels);
-    const counts = @json($counts);
-
-    console.log('Labels:', labels);
-    console.log('Counts:', counts);
-
-    const chartOptions = {
-        chart: {
-            type: 'bar'
-            , height: 350
-        }
-        , series: [{
-            name: 'Jumlah'
-            , data: counts
-        }]
-        , xaxis: {
-            categories: labels
-        }
-    };
-</script>
