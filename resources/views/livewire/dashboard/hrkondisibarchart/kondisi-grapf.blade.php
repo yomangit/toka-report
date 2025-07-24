172c4041fe7af6ef51b8e67badddd28b79b94326
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-24 15:18:36 +0800

    assa

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -77,81 +77,28 @@
         , chartKondisi
     );
     kondisiChart.render();
-  window.Livewire.dispatch('hazardChartShouldRefresh')
 
+    // Simulasi update chart tiap 5 detik
     setInterval(() => {
-        // Initial data from backend
-        const initialLabels = @json($labels);
-        const initialCounts = @json($counts);
+        // Fetch data baru dari backend di sini jika perlu pakai fetch/ajax
+        // Untuk contoh, kita pakai dummy data random
+        const newCounts = initialCounts.map(() => Math.floor(Math.random() * 10) + 1);
 
-        function shortenLabels(labels) {
-            return labels.map(label =>
-                label.length > 20 ? label.slice(0, 20) + '…' : label
-            );
-        }
+        kondisiChart.updateSeries([{
+            name: 'Jumlah'
+            , data: newCounts
+        }]);
 
-        function generateColors(length) {
-            const colorList = ['#FF4560', '#008FFB', '#00E396', '#FEB019', '#775DD0', '#FF66C3', '#546E7A', '#26a69a', '#D10CE8'];
-            return Array.from({
-                length
-            }, (_, i) => colorList[i % colorList.length]);
-        }
+        // Jika juga mau update label (opsional)
+        // const newLabels = ['Label1', 'Label2', ...];
+        // kondisiChart.updateOptions({
+        //     xaxis: {
+        //         categories: shortenLabels(newLabels)
+        //     },
+        //     colors: generateColors(newLabels.length)
+        // });
 
-        const chartKondisi = {
-            chart: {
-                type: 'bar'
-                , height: 350
-            }
-            , series: [{
-                name: 'Jumlah'
-                , data: initialCounts
-            }]
-            , colors: generateColors(initialLabels.length)
-            , title: {
-                text: 'Kondisi Tidak Aman'
-                , align: 'center'
-                , style: {
-                    fontSize: '12px'
-                    , fontWeight: 'bold'
-                    , color: '#fb7185'
-                }
-            }
-            , xaxis: {
-                categories: shortenLabels(initialLabels)
-                , labels: {
-                    rotate: -45
-                    , style: {
-                        fontSize: '09px'
-                    }
-                }
-            }
-            , plotOptions: {
-                bar: {
-                    borderRadius: 4
-                    , distributed: true
-                }
-            }
-            , fill: {
-                type: 'gradient'
-                , gradient: {
-                    shade: 'light'
-                    , type: 'vertical'
-                    , shadeIntensity: 0.25
-                    , inverseColors: true
-                    , opacityFrom: 0.9
-                    , opacityTo: 1
-                    , stops: [50, 100]
-                }
-            }
-        };
-
-        const kondisiChart = new ApexCharts(
-            document.querySelector("#kondisiCharts")
-            , chartKondisi
-        );
-        kondisiChart.render();
-
-    }, 5000);
+    }, 5000); // 5000 ms = 5 detik
 
 </script>
 @endpush
