deba11395265ac0b4e9aa905e743fe68f0989d92
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-24 08:32:03 +0800

    asa

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -5,82 +5,16 @@
         const labels = @json($labels);
         const counts = @json($counts);
         var chartKondisi = {
-            series: [{
+            chart: {
+                type: 'bar'
+                , height: 350
+            }
+            , series: [{
                 name: 'Jumlah'
                 , data: counts
             }]
-            , chart: {
-                height: 350
-                , type: 'bar'
-            , }
-            , plotOptions: {
-                bar: {
-                    borderRadius: 10
-                    , dataLabels: {
-                        position: 'top', // top, center, bottom
-                    }
-                , }
-            }
-            , dataLabels: {
-                enabled: true
-                , formatter: function(val) {
-                    return val + "%";
-                }
-                , offsetY: -20
-                , style: {
-                    fontSize: '12px'
-                    , colors: ["#304758"]
-                }
-            },
-
-            xaxis: {
+            , xaxis: {
                 categories: labels
-                , position: 'top'
-                , axisBorder: {
-                    show: false
-                }
-                , axisTicks: {
-                    show: false
-                }
-                , crosshairs: {
-                    fill: {
-                        type: 'gradient'
-                        , gradient: {
-                            colorFrom: '#D8E3F0'
-                            , colorTo: '#BED1E6'
-                            , stops: [0, 100]
-                            , opacityFrom: 0.4
-                            , opacityTo: 0.5
-                        , }
-                    }
-                }
-                , tooltip: {
-                    enabled: true
-                , }
-            }
-            , yaxis: {
-                axisBorder: {
-                    show: false
-                }
-                , axisTicks: {
-                    show: false
-                , }
-                , labels: {
-                    show: false
-                    , formatter: function(val) {
-                        return val + "%";
-                    }
-                }
-
-            }
-            , title: {
-                text: 'Monthly Inflation in Argentina, 2002'
-                , floating: true
-                , offsetY: 330
-                , align: 'center'
-                , style: {
-                    color: '#444'
-                }
             }
         };
 
