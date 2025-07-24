113fee85377f4fd8615c4b8e1a5f9ec99cfb68c3
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-24 15:42:29 +0800

    as

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -72,21 +72,23 @@
 
     window.addEventListener('kondisiChartUpdated', (event) => {
         const data = event.detail;
-        const newLabels = shortenLabels(data.labels);
-        const newCounts = data.counts;
-        const newColors = generateColors(newLabels.length);
+        console.log(data);
+        
+        // const newLabels = shortenLabels(data.labels);
+        // const newCounts = data.counts;
+        // const newColors = generateColors(newLabels.length);
 
-        kondisiChart.updateOptions({
-            xaxis: {
-                categories: newLabels
-            },
-            colors: newColors
-        });
+        // kondisiChart.updateOptions({
+        //     xaxis: {
+        //         categories: newLabels
+        //     },
+        //     colors: newColors
+        // });
 
-        kondisiChart.updateSeries([{
-            name: 'Jumlah',
-            data: newCounts
-        }]);
+        // kondisiChart.updateSeries([{
+        //     name: 'Jumlah',
+        //     data: newCounts
+        // }]);
     });
 </script>
 @endpush
