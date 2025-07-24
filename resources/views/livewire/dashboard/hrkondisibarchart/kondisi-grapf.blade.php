0fdabe068f0677100a7c0f7cc30bb8bd90b7e9d1
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-24 16:00:55 +0800

    sas

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -16,53 +16,55 @@
 
     function generateColors(length) {
         const colorList = ['#FF4560', '#008FFB', '#00E396', '#FEB019', '#775DD0', '#FF66C3', '#546E7A', '#26a69a', '#D10CE8'];
-        return Array.from({ length }, (_, i) => colorList[i % colorList.length]);
+        return Array.from({
+            length
+        }, (_, i) => colorList[i % colorList.length]);
     }
 
     const chartKondisi = {
         chart: {
-            type: 'bar',
-            height: 350
-        },
-        series: [{
-            name: 'Jumlah',
-            data: initialCounts
-        }],
-        colors: generateColors(initialLabels.length),
-        title: {
-            text: 'Kondisi Tidak Aman',
-            align: 'center',
-            style: {
-                fontSize: '12px',
-                fontWeight: 'bold',
-                color: '#fb7185'
+            type: 'bar'
+            , height: 350
+        }
+        , series: [{
+            name: 'Jumlah'
+            , data: initialCounts
+        }]
+        , colors: generateColors(initialLabels.length)
+        , title: {
+            text: 'Kondisi Tidak Aman'
+            , align: 'center'
+            , style: {
+                fontSize: '12px'
+                , fontWeight: 'bold'
+                , color: '#fb7185'
             }
-        },
-        xaxis: {
-            categories: shortenLabels(initialLabels),
-            labels: {
-                rotate: -45,
-                style: {
+        }
+        , xaxis: {
+            categories: shortenLabels(initialLabels)
+            , labels: {
+                rotate: -45
+                , style: {
                     fontSize: '09px'
                 }
             }
-        },
-        plotOptions: {
+        }
+        , plotOptions: {
             bar: {
-                borderRadius: 4,
-                distributed: true
+                borderRadius: 4
+                , distributed: true
             }
-        },
-        fill: {
-            type: 'gradient',
-            gradient: {
-                shade: 'light',
-                type: 'vertical',
-                shadeIntensity: 0.25,
-                inverseColors: true,
-                opacityFrom: 0.9,
-                opacityTo: 1,
-                stops: [50, 100]
+        }
+        , fill: {
+            type: 'gradient'
+            , gradient: {
+                shade: 'light'
+                , type: 'vertical'
+                , shadeIntensity: 0.25
+                , inverseColors: true
+                , opacityFrom: 0.9
+                , opacityTo: 1
+                , stops: [50, 100]
             }
         }
     };
@@ -70,25 +72,25 @@
     const kondisiChart = new ApexCharts(document.querySelector("#kondisiCharts"), chartKondisi);
     kondisiChart.render();
 
-    window.addEventListener('kondisiChartUpdated', (event) => {
-        const data = event.detail;
-        
-        console.log(data.labels);
+    Livewire.on('kondisiChartUpdated', (data) => {
+        console.log("Updated labels:", data.labels); // ✅ sekarang ini HARUS tampil
+
         const newLabels = shortenLabels(data.labels);
-        // const newCounts = data.counts;
-        // const newColors = generateColors(newLabels.length);
+        const newCounts = data.counts;
+        const newColors = generateColors(newLabels.length);
 
-        // kondisiChart.updateOptions({
-        //     xaxis: {
-        //         categories: newLabels
-        //     },
-        //     colors: newColors
-        // });
+        kondisiChart.updateOptions({
+            xaxis: {
+                categories: newLabels
+            }
+            , colors: newColors
+        });
 
-        // kondisiChart.updateSeries([{
-        //     name: 'Jumlah',
-        //     data: newCounts
-        // }]);
+        kondisiChart.updateSeries([{
+            name: 'Jumlah'
+            , data: newCounts
+        }]);
     });
+
 </script>
 @endpush
