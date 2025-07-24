ea4172b74f6aed4d9b0233127a8230221ae78ebe
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-24 15:57:06 +0800

    sdds

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -71,7 +71,7 @@
     kondisiChart.render();
 
     window.addEventListener('kondisiChartUpdated', (event) => {
-        const data = event.detail[0];
+        const data = event.detail;
         
         console.log(data.labels);
         const newLabels = shortenLabels(data.labels);
