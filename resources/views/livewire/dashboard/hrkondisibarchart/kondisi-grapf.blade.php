1a193cbddbe28560500fcf3b7b264cd8cc7f8a61
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-24 08:34:26 +0800

    asdfa

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -4,17 +4,45 @@
     <script type="text/javascript">
         const labels = @json($labels);
         const counts = @json($counts);
-        var chartKondisi = {
+
+        const shortLabels = labels.map(label => label.length > 10 ? label.slice(0, 10) + '…' : label);
+
+        const chartKondisi = {
             chart: {
                 type: 'bar'
                 , height: 350
             }
+            , colors: labels.map((_, i) => {
+                const colorList = ['#FF4560', '#008FFB', '#00E396', '#FEB019', '#775DD0', '#FF66C3', '#546E7A', '#26a69a', '#D10CE8'];
+                return colorList[i % colorList.length];
+            })
             , series: [{
                 name: 'Jumlah'
                 , data: counts
             }]
             , xaxis: {
-                categories: labels
+                categories: shortLabels
+                , labels: {
+                    rotate: -45
+                }
+            }
+            , plotOptions: {
+                bar: {
+                    borderRadius: 4
+                    , distributed: true
+                }
+            }
+            , fill: {
+                type: 'gradient'
+                , gradient: {
+                    shade: 'light'
+                    , type: 'vertical'
+                    , shadeIntensity: 0.25
+                    , inverseColors: true
+                    , opacityFrom: 0.9
+                    , opacityTo: 1
+                    , stops: [50, 100]
+                }
             }
         };
 
