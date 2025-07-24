76a3ce47a5e468b6c5e1b52d7a72d746585b947e
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-24 08:16:34 +0800

    ewe

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/apexchart.js',
+                'resources/js/kondisichart.js',
             ],
             refresh: true,
         }),
