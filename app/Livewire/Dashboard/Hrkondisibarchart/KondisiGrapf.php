e72447b473b98fc3fb63fe4fa56453ca7daed131
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-24 16:12:39 +0800

    asassaa

--- app/Livewire/Dashboard/Hrkondisibarchart/KondisiGrapf.php
+++ app/Livewire/Dashboard/Hrkondisibarchart/KondisiGrapf.php
@@ -34,11 +34,8 @@ class KondisiGrapf extends Component
 
         $this->labels = $reports->map(fn($item) => optional($item->kondisiTidakAman)?->name ?? 'Unknown')->toArray();
         $this->counts = $reports->pluck('total')->toArray();
-        
-        $this->dispatch('kondisiChartUpdated', [
-            'labels' => $this->labels,
-            'counts' => $this->counts
-        ]);
+
+        $this->dispatch('kondisiChartUpdated', labels: $this->labels, counts: $this->counts);
     }
     public function render()
     {
