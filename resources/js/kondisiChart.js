6d6f85623f055d81190b18d3d2d3e9314b392229
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-24 09:21:29 +0800

    df

--- resources/js/kondisiChart.js
+++ resources/js/kondisiChart.js
@@ -1,4 +1,7 @@
 import ApexCharts from "apexcharts";
 
-var kondisiChart = new ApexCharts(document.querySelector("#kondisiCharts"),chartKondisi);
+var kondisiChart = new ApexCharts(
+    document.querySelector("#kondisiCharts"),
+    chartKondisi
+);
 kondisiChart.render();
