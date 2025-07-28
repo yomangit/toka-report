a544c21df4873393c1a92df19c471cd071e56189
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-28 15:09:26 +0800

    saas

--- app/Livewire/Admin/PersonInCharge/Pic.php
+++ app/Livewire/Admin/PersonInCharge/Pic.php
@@ -93,7 +93,7 @@ class Pic extends Component
     }
     public function resetForm()
     {
-        $this->reset(['showModal', 'editMode', 'divisionId', 'selectedUsers', 'searchResults', 'showUserDropdown']);
+        $this->reset(['showModal', 'editMode', 'divisionId', 'selectedUsers', 'searchDivisionQuery', 'showDivisionDropdown']);
     }
     public function render()
     {
