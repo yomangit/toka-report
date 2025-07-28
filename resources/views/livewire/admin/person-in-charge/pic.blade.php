307fb6608e8c3693138d5cb9120fe3d5087e1fab
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-28 21:56:43 +0800

    sa

--- resources/views/livewire/admin/person-in-charge/pic.blade.php
+++ resources/views/livewire/admin/person-in-charge/pic.blade.php
@@ -6,7 +6,7 @@
     </div>
 
     <div class="overflow-x-auto">
-        <table class="min-w-full text-sm table-auto">
+        <table class="table min-w-full text-sm table-auto table-xs">
             <thead>
                 <tr class="text-left bg-gray-100">
                     <th class="px-4 py-2">Divisi</th>
