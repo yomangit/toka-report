c528a5b4da5cd54c344f9a3e898e7917b2fdbb84
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-30 16:10:13 +0800

    dssd

--- resources/views/livewire/notification-manager.blade.php
+++ resources/views/livewire/notification-manager.blade.php
@@ -12,7 +12,7 @@
         console.log("Player ID:", playerId);
         if (playerId) {
             Livewire.dispatch('userSubscribed', {
-                player_id: 'playerId'
+                player_id: playerId
             });
         } else {
             alert("Gagal mendapatkan Player ID setelah permintaan izin.");
