420eca4d6a610e09a7751a713df611c376816753
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-30 15:51:01 +0800

    sss

--- resources/views/livewire/notification-manager.blade.php
+++ resources/views/livewire/notification-manager.blade.php
@@ -1,32 +1,24 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
 </div>
-<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" async></script>
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
 <script>
     window.OneSignalDeferred = window.OneSignalDeferred || [];
-    OneSignalDeferred.push(async function (OneSignal) {
+    OneSignalDeferred.push(async function(OneSignal) {
         await OneSignal.init({
-         appId: "b50c5099-e9f4-439d-a8e9-319b0e4e5e18", // ← ganti dengan milikmu
-        });
-
-        // Minta izin notifikasi
-        const permission = await OneSignal.Notifications.requestPermission();
-        console.log('🔐 Izin:', permission); // 'granted'
-
-        // Ambil player ID setelah diizinkan
-        const playerId = await OneSignal.User.PushSubscription.id;// SDK v16: pakai async
-        console.log("📥 playerId:", playerId);
-
+            appId: "b50c5099-e9f4-439d-a8e9-319b0e4e5e18"
+        , });
+        const playerId = OneSignal.User.PushSubscription.id;
+        console.log("Player ID:", playerId);
         if (playerId) {
-            Livewire.hook('component.initialized', (component) => {
-                if (component.name === 'notification-manager') {
-                    Livewire.dispatch('userSubscribed', {
-                        player_id: playerId
-                    });
-                }
-            });
+            // Livewire.dispatch('userSubscribed', {
+            //     // player_id: playerId
+            //     player_id: 'playerId'
+            // });
+              @this.set('description', playerId);
         } else {
-            console.warn("⚠️ Player ID belum tersedia setelah init.");
+            alert("Gagal mendapatkan Player ID setelah permintaan izin.");
         }
     });
 </script>
+
