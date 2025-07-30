7e9c8baeb67b88ab0fdf529d18d1f78a28ba9dde
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-30 09:54:12 +0800

    ssss

--- resources/views/livewire/notification-manager.blade.php
+++ resources/views/livewire/notification-manager.blade.php
@@ -2,7 +2,7 @@
     {{-- Komponen NotificationManager --}}
 
     <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-    {{-- <script>
+    <script>
         window.OneSignalDeferred = window.OneSignalDeferred || [];
         OneSignalDeferred.push(async function(OneSignal) {
             await OneSignal.init({
@@ -36,30 +36,6 @@
             });
         });
 
-    </script> --}}
-    <script>
-        window.OneSignalDeferred = window.OneSignalDeferred || [];
-        OneSignalDeferred.push(function(OneSignal) {
-            OneSignal.init({
-                appId: "b50c5099-e9f4-439d-a8e9-319b0e4e5e18"
-                , notifyButton: {
-                    enable: true
-                }
-            , });
-
-            OneSignal.Notifications.addEventListener('permissionChange', function(state) {
-                if (state === 'granted') {
-                    OneSignal.getUserId().then(function(playerId) {
-                         console.log("✅ Already subscribed. Player ID:", playerId);
-                        if (playerId) {
-                            Livewire.dispatch('userSubscribed', {
-                                player_id: playerId
-                            });
-                        }
-                    });
-                }
-            });
-        });
-
     </script>
+
 </div>
