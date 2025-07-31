07d8ffb384e2c8b39304257c97f4e7a0fb5c2f5c
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-31 08:10:10 +0800

    sas

--- app/Livewire/NotificationManager.php
+++ app/Livewire/NotificationManager.php
@@ -10,10 +10,12 @@ class NotificationManager extends Component
     #[On('userSubscribed')]
     public function simpanPlayerId($player_id)
     {
-        // Contoh: simpan ke user
-        auth()->user()->update([
-            'onesignal_player_id' => $player_id
-        ]);
+        if (auth()) {
+            // Contoh: simpan ke user
+            auth()->user()->update([
+                'onesignal_player_id' => $player_id
+            ]);
+        }
     }
     public function render()
     {
