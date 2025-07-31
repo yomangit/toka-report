8cfdaa57a19152f195cb45a59799ee4edbc500a7
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-31 16:09:32 +0800

    xzxz

--- app/Livewire/EventReport/HazardReportGuest/Create.php
+++ app/Livewire/EventReport/HazardReportGuest/Create.php
@@ -506,10 +506,6 @@ class Create extends Component
                 'line3'     => 'Terima kasih atas perhatian dan kerjasamanya 🙏',
                 'actionUrl' => url("/eventReport/hazardReportDetail/{$url}"),
             ]));
-
-            $judul = '⚠️ Laporan Bahaya dengan Nomor Referensi: ' . $this->reference;
-            $isi = $this->report_byName . ' telah mengirimkan laporan bahaya kepada Anda. Mohon untuk segera ditinjau.';
-            $url = url("/eventReport/hazardReportDetail/{$url}");
             $restApiKey = 'os_v2_app_wugfbgpj6rbz3khjggnq4ts6ddon2ktn3fbeuduhkekgx7odctgn7qfesyj4r4hcd7fjar4cidqbkmkqqna7h26oug3wnyxomvqfvni';
             $appId = 'b50c5099-e9f4-439d-a8e9-319b0e4e5e18';
             $user_os = User::find($this->report_to);
@@ -526,7 +522,7 @@ class Create extends Component
 
             $judul = '⚠️ Laporan Bahaya dengan Nomor Referensi: ' . $this->reference;
             $isi = $this->report_byName . ' telah mengirimkan laporan bahaya kepada Anda. Mohon untuk segera ditinjau.';
-            $url = url("/eventReport/hazardReportDetail/{$this->report_id}");
+            $url = url("/eventReport/hazardReportDetail/{$url}");
 
             $response = Http::withHeaders([
                 'Authorization' => 'Basic ' . $restApiKey,
