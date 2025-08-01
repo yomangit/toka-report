be66337b83d791e0da66f73dd323803c5c9400ac
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-01 15:12:55 +0800

    zxcvb

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,5 +1,25 @@
-<div class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40" x-data="{ open: @entangle($attributes->wire('model')) }" x-show="open">
-    <div @click.away="open = false" class="w-full max-w-lg p-6 rounded shadow-lg bg-base-300">
+<div
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40"
+    x-data="{
+        open: @entangle($attributes->wire('model')),
+        init() {
+            document.addEventListener('keydown', (e) => {
+                // Nonaktifkan tombol Escape
+                if (e.key === 'Escape') e.preventDefault();
+            });
+        }
+    }"
+    x-init="init"
+    x-show="open"
+    x-transition:enter="transition ease-out duration-200"
+    x-transition:enter-start="opacity-0 scale-95"
+    x-transition:enter-end="opacity-100 scale-100"
+    x-transition:leave="transition ease-in duration-150"
+    x-transition:leave-start="opacity-100 scale-100"
+    x-transition:leave-end="opacity-0 scale-95"
+    style="display: none;"
+>
+    <div class="w-full max-w-lg p-6 rounded shadow-lg bg-base-300">
         <div class="text-lg font-semibold">
             {{ $title }}
         </div>
