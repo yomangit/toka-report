e8fa1628695b87f7ae98f55181e10f13d1add6d2
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-04 13:21:08 +0800

    asas

--- app/Helpers/NotificationHelper.php
+++ app/Helpers/NotificationHelper.php
@@ -17,8 +17,8 @@ class NotificationHelper
         if (empty($playerIds)) {
             return;
         }
-      $response =  Http::withHeaders([
-            'Authorization' => 'Basic ' . 'os_v2_app_wugfbgpj6rbz3khjggnq4ts6ddon2ktn3fbeuduhkekgx7odctgn7qfesyj4r4hcd7fjar4cidqbkmkqqna7h26oug3wnyxomvqfvni',
+        $response =  Http::withHeaders([
+            'Authorization' => 'Basic ' . config('services.onesignal.rest_api_key'),
             'Content-Type' => 'application/json',
         ])->post('https://onesignal.com/api/v1/notifications', [
             'app_id' => 'b50c5099-e9f4-439d-a8e9-319b0e4e5e18',
