dd0ac6e8c5ea9132a1344a92a5fa34ed936f4686
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-04 12:45:30 +0800

    sass

--- app/Helpers/NotificationHelper.php
+++ app/Helpers/NotificationHelper.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Http;
 
 class NotificationHelper
 {
-    public static function sendToUser($user,  $heading,  $content, $url): void
+    public static function sendToUser($user,  $heading,  $content, $url)
     {
         $playerIds = $user->oneSignalPlayers()->pluck('player_id')
             ->filter()
@@ -17,7 +17,7 @@ class NotificationHelper
         if (empty($playerIds)) {
             return;
         }
-        Http::withHeaders([
+      $response =  Http::withHeaders([
             'Authorization' => 'Basic ' . 'os_v2_app_wugfbgpj6rbz3khjggnq4ts6ddon2ktn3fbeuduhkekgx7odctgn7qfesyj4r4hcd7fjar4cidqbkmkqqna7h26oug3wnyxomvqfvni',
             'Content-Type' => 'application/json',
         ])->post('https://onesignal.com/api/v1/notifications', [
@@ -27,5 +27,6 @@ class NotificationHelper
             'contents' => $content,
             'url' =>  $url,
         ]);
+        return $response->json();
     }
 }
