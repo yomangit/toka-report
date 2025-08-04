08cfe6744d223d98851d3109866b0135961e005b
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-04 14:04:06 +0800

    sas

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -50,6 +50,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/')->with('logout', true);
+        return redirect('/login')->with('logout', true);
     }
 }
