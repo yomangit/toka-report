a5d61806b7cbbfa4aa21d095f34f2611340762ba
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-14 07:49:30 +0800

    ds

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -116,30 +116,30 @@
     const kondisiChart = new ApexCharts(document.querySelector("#kondisiCharts"), chartKondisi);
     kondisiChart.render();
     // Listener Livewire event
-    Livewire.on('kondisiChartUpdated', event => {
-        const data = event; // di Livewire 3 biasanya langsung data array, tapi kadang terbungkus
-        console.log(data);
+    // Livewire.on('kondisiChartUpdated', event => {
+    //     const data = event; // di Livewire 3 biasanya langsung data array, tapi kadang terbungkus
+    //     console.log(data);
         
 
-        if (!Array.isArray(data)) {
-            console.warn("Data chart tidak valid:", data);
-            return;
-        }
-        const newLabels = event[0].map(item => item.label);
-        const newCounts = event[0].map(item => item.count);
+    //     if (!Array.isArray(data)) {
+    //         console.warn("Data chart tidak valid:", data);
+    //         return;
+    //     }
+    //     const newLabels = event[0].map(item => item.label);
+    //     const newCounts = event[0].map(item => item.count);
 
-        kondisiChart.updateOptions({
-            xaxis: {
-                categories: shortenLabels(newLabels)
-            }
-            , colors: '#00E396'
-        });
+    //     kondisiChart.updateOptions({
+    //         xaxis: {
+    //             categories: shortenLabels(newLabels)
+    //         }
+    //         , colors: '#00E396'
+    //     });
 
-        kondisiChart.updateSeries([{
-            name: 'Jumlah'
-            , data: newCounts
-        }]);
-    });
+    //     kondisiChart.updateSeries([{
+    //         name: 'Jumlah'
+    //         , data: newCounts
+    //     }]);
+    // });
 
 </script>
 @endpush
