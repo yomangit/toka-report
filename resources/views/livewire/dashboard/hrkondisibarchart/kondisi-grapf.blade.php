1e8d92ae5f59821ed1769da97e02b11fdbd1ba04
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-13 11:34:01 +0800

    dsd

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -4,50 +4,52 @@
 </div>
 
 @push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 <script>
-         flatpickr("#rangeDate", {
-            mode: 'range',
-            dateFormat: "d-m-Y", //defaults to "F Y"
-            onChange: function(dates) {
-                if (dates.length === 2) {
+    flatpickr("#rangeDate", {
+        mode: 'range'
+        , dateFormat: "d-m-Y", //defaults to "F Y"
+        onChange: function(dates) {
+            if (dates.length === 2) {
 
-                    var start = new Date(dates[0]);
-                    var end = new Date(dates[1]);
+                var start = new Date(dates[0]);
+                var end = new Date(dates[1]);
 
-                    var year = start.getFullYear();
-                    var month = start.getMonth() + 1;
-                    var dt = start.getDate();
+                var year = start.getFullYear();
+                var month = start.getMonth() + 1;
+                var dt = start.getDate();
 
-                    if (dt < 10) {
-                        dt = '0' + dt;
-                    }
-                    if (month < 10) {
-                        month = '0' + month;
-                    }
-                    var year2 = end.getFullYear();
-                    var month2 = end.getMonth() + 1;
-                    var dt2 = end.getDate();
-
-                    if (dt2 < 10) {
-                        dt2 = '0' + dt2;
-                    }
-                    if (month2 < 10) {
-                        month2 = '0' + month2;
-                    }
-
-                    // var tglMulai = year + '-' + month + '-' + dt;
-                    // var tglAkhir = year2 + '-' + month2 + '-' + dt2;
-
-                    var tglMulai = year + '-' + month + '-' + dt;
-                    var tglAkhir = year2 + '-' + month2 + '-' + dt2;
-                    @this.set('tglMulai', tglMulai)
-                    @this.set('tglAkhir', tglAkhir)
+                if (dt < 10) {
+                    dt = '0' + dt;
                 }
+                if (month < 10) {
+                    month = '0' + month;
+                }
+                var year2 = end.getFullYear();
+                var month2 = end.getMonth() + 1;
+                var dt2 = end.getDate();
+
+                if (dt2 < 10) {
+                    dt2 = '0' + dt2;
+                }
+                if (month2 < 10) {
+                    month2 = '0' + month2;
+                }
+
+                // var tglMulai = year + '-' + month + '-' + dt;
+                // var tglAkhir = year2 + '-' + month2 + '-' + dt2;
+
+                var tglMulai = year + '-' + month + '-' + dt;
+                var tglAkhir = year2 + '-' + month2 + '-' + dt2;
+                @this.set('tglMulai', tglMulai)
+                @this.set('tglAkhir', tglAkhir)
             }
-        });
-    </script>
+        }
+    });
+
+</script>
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <script>
     const initialLabels = @json($labels);
     const initialCounts = @json($counts);
@@ -115,7 +117,6 @@
 
     const kondisiChart = new ApexCharts(document.querySelector("#kondisiCharts"), chartKondisi);
     kondisiChart.render();
-
 
 </script>
 @endpush
