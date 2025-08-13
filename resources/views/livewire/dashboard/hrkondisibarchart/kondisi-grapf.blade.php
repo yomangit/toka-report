6b9b3e34c5e336847e10adf270280464e0ad8443
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-13 15:11:32 +0800

    ssas

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -87,7 +87,7 @@
             xaxis: {
                 categories: shortenLabels(newLabels)
             }
-            , colors: 
+            , colors: '#00E396'
         });
 
         kondisiChart.updateSeries([{
