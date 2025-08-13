19e9cb8551cb3283df8f395d0cad43050c64b04f
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-14 07:58:32 +0800

    asas

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -116,30 +116,10 @@
     const kondisiChart = new ApexCharts(document.querySelector("#kondisiCharts"), chartKondisi);
     kondisiChart.render();
     // Listener Livewire event
-    // Livewire.on('kondisiChartUpdated', event => {
-    //     const data = event; // di Livewire 3 biasanya langsung data array, tapi kadang terbungkus
-    //     console.log(data);
-        
-
-    //     if (!Array.isArray(data)) {
-    //         console.warn("Data chart tidak valid:", data);
-    //         return;
-    //     }
-    //     const newLabels = event[0].map(item => item.label);
-    //     const newCounts = event[0].map(item => item.count);
-
-    //     kondisiChart.updateOptions({
-    //         xaxis: {
-    //             categories: shortenLabels(newLabels)
-    //         }
-    //         , colors: '#00E396'
-    //     });
-
-    //     kondisiChart.updateSeries([{
-    //         name: 'Jumlah'
-    //         , data: newCounts
-    //     }]);
-    // });
+    Livewire.on('kondisiChartUpdated', event => {
+        const data = event; // di Livewire 3 biasanya langsung data array, tapi kadang terbungkus
+        console.log(data);
+    });
 
 </script>
 @endpush
