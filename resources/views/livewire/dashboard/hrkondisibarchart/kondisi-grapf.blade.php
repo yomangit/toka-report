40d6e60021d7b3a5f1003eeeb287a0db8a04e177
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-14 12:37:01 +0800

    dsd

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -1,6 +1,6 @@
 <div wire:init="loadChartData">
     <x-input-daterange id="rangeDate" placeholder='date-range' />
-    <div id="kondisiCharts"></div>
+    <div wire:ignore id="kondisiCharts"></div>
 </div>
 
 @push('scripts')
@@ -107,55 +107,9 @@
     kondisiChart.render();
     Livewire.on('berhasilUpdate', jsonString => {
         const apexData = JSON.parse(jsonString);
-
-        const newchartKondisi = {
-            chart: {
-                type: 'bar'
-                , height: 350
-            }
-            , series: [{
-                name: 'Jumlah'
-                , data: apexData.count
-            }]
-            , title: {
-                text: 'Kondisi Tidak Aman'
-                , align: 'center'
-                , style: {
-                    fontSize: '12px'
-                    , fontWeight: 'bold'
-                    , color: '#fb7185'
-                }
-            }
-            , xaxis: {
-                categories: shortenLabels(apexData.label)
-                , labels: {
-                    rotate: -45
-                    , style: {
-                        fontSize: '09px'
-                    }
-                }
-            }
-            , plotOptions: {
-                bar: {
-                    borderRadius: 4
-                    , distributed: true
-                }
-            }
-            , fill: {
-                type: 'gradient'
-                , gradient: {
-                    shade: 'light'
-                    , type: 'vertical'
-                    , shadeIntensity: 0.25
-                    , inverseColors: true
-                    , opacityFrom: 0.9
-                    , opacityTo: 1
-                    , stops: [50, 100]
-                }
-            }
-        };
-        const newkondisiChart = new ApexCharts(document.querySelector("#kondisiCharts"), newchartKondisi);
-        newkondisiChart.render();
+        console.log(apexData);
+        
+       
     });
 
 </script>
