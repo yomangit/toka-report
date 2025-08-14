b9dd22a3df97df4be7cb7ac961aa2b21d6746b58
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-14 14:10:54 +0800

    sss

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -4,44 +4,57 @@
 
    @push('scripts')
    <script src="https://echarts.apache.org/en/js/vendors/echarts/dist/echarts.min.js"></script>
+   <script src="https://echarts.apache.org/en/js/vendors/echarts-gl/dist/echarts-gl.min.js"></script>
    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
    <script>
        const data = JSON.parse('<?php echo $kondisi ?>');
        var dom = document.getElementById('chart-container');
-       var myChart = echarts.init(dom, null, {
-           renderer: 'canvas'
-           , useDirtyRect: false
-       });
-       var app = {};
+       var myChart = echarts.init(dom);
 
-
-       var option;
-
-       option = {
-           xAxis: {
+       var option = {
+           tooltip: {}
+           , visualMap: {
+               max: Math.max(...data.count)
+               , inRange: {
+                   color: ['#4CAF50', '#FFC107', '#F44336'] // gradasi warna
+               }
+           }
+           , xAxis3D: {
                type: 'category'
                , data: data.label
            }
-           , yAxis: {
+           , yAxis3D: {
+               type: 'category'
+               , data: ['Jumlah'] // bisa 1 kategori karena ini bar 3D tunggal
+           }
+           , zAxis3D: {
                type: 'value'
            }
+           , grid3D: {
+               boxWidth: 200
+               , boxDepth: 50
+               , viewControl: {
+                   projection: 'perspective'
+               }
+           }
            , series: [{
-               data: data.count
-               , type: 'bar'
+               type: 'bar3D'
+               , data: data.label.map((label, i) => [label, 'Jumlah', data.count[i]])
+               , shading: 'color'
                , label: {
                    show: true
-                   , position: 'inside'
+                   , formatter: '{c}'
                }
-           , }],
-
+               , itemStyle: {
+                   opacity: 0.9
+               }
+           }]
        };
 
-
-       if (option && typeof option === 'object') {
-           myChart.setOption(option);
-       }
-
+       myChart.setOption(option);
        window.addEventListener('resize', myChart.resize);
 
    </script>
+
    @endpush
