1b6b5b4bfc3b8546c2c54df54a6f8a04e2849f20
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-14 12:34:19 +0800

    asas

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -108,15 +108,54 @@
     Livewire.on('berhasilUpdate', jsonString => {
         const apexData = JSON.parse(jsonString);
 
-        kondisiChart.updateOptions({
-            xaxis: {
-                categories: shortenLabels(apexData.label)
+         const newchartKondisi = {
+        chart: {
+            type: 'bar'
+            , height: 350
+        }
+        , series: [{
+            name: 'Jumlah'
+            , data: apexData.count
+        }]
+        , title: {
+            text: 'Kondisi Tidak Aman'
+            , align: 'center'
+            , style: {
+                fontSize: '12px'
+                , fontWeight: 'bold'
+                , color: '#fb7185'
             }
-        });
-        kondisiChart.updateSeries([{
-            name: 'Jumlah',
-            data: apexData.count
-        }]);
+        }
+        , xaxis: {
+            categories: shortenLabels(apexData.label)
+            , labels: {
+                rotate: -45
+                , style: {
+                    fontSize: '09px'
+                }
+            }
+        }
+        , plotOptions: {
+            bar: {
+                borderRadius: 4
+                , distributed: true
+            }
+        }
+        , fill: {
+            type: 'gradient'
+            , gradient: {
+                shade: 'light'
+                , type: 'vertical'
+                , shadeIntensity: 0.25
+                , inverseColors: true
+                , opacityFrom: 0.9
+                , opacityTo: 1
+                , stops: [50, 100]
+            }
+        }
+    };
+    const newkondisiChart = new ApexCharts(document.querySelector("#kondisiCharts"), newchartKondisi);
+    newkondisiChart.render();
     });
 
 </script>
