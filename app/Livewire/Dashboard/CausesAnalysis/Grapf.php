d64ddc62bf58f2761f849f4b7104c774728a4abb
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-14 16:22:51 +0800

    dsd

--- app/Livewire/Dashboard/CausesAnalysis/Grapf.php
+++ app/Livewire/Dashboard/CausesAnalysis/Grapf.php
@@ -4,25 +4,55 @@ namespace App\Livewire\Dashboard\CausesAnalysis;
 
 use Livewire\Component;
 use App\Models\HazardReport;
+use Illuminate\Support\Facades\Auth;
 
 class Grapf extends Component
 {
     public $labels = [];
     public $counts = [];
+    public $pie;
+    public $tglMulai;
+    public $tglAkhir;
     protected $listeners = ['refreshPerbandinganChart' => 'updatePerbandinganData'];
 
     public function mount()
     {
         $this->updatePerbandinganData(); // inisialisasi pertama
     }
-
+    public function updated($property)
+    {
+        if (in_array($property, ['tglMulai', 'tglAkhir'])) {
+            $this->loadChartData();
+        }
+    }
     public function updatePerbandinganData()
     {
-        $totalKondisi = HazardReport::whereNotNull('kondisitidakamen_id')->count();
-        $totalTindakan = HazardReport::whereNotNull('tindakantidakamen_id')->count();
+        $user = Auth::user();
+        $totalKondisi = HazardReport::whereNotNull('kondisitidakamen_id');
+        $totalTindakan = HazardReport::whereNotNull('tindakantidakamen_id');
 
-        $this->labels = ['Kondisi Tidak Aman', 'Tindakan Tidak Aman'];
-        $this->counts = [$totalKondisi, $totalTindakan];
+        if ($this->tglMulai && $this->tglAkhir) {
+            $totalKondisi->whereBetween('date', [$this->tglMulai, $this->tglAkhir]);
+            $totalTindakan->whereBetween('date', [$this->tglMulai, $this->tglAkhir]);
+        }
+
+        if ($user->hasRolePermit('administration')) {
+            $kondisi = $totalKondisi->count();
+            $tindakan = $totalTindakan->count();
+        } elseif ($user->hasRolePermit('auth') && $user->divisions()->exists()) {
+            $divisionIds = $user->divisions->pluck('id')->toArray();
+            $kondisi = $totalKondisi->whereIn('division_id', $divisionIds)->count();
+            $tindakan = $totalTindakan->whereIn('division_id', $divisionIds)->count();
+        } else {
+            $kondisi = collect();
+            $tindakan = collect();
+        }
+        $data = [
+            'label' => ['Kondisi Tidak Aman', 'Tindakan Tidak Aman'],
+            'count' => [$kondisi, $tindakan]
+        ];
+        $this->pie = json_encode($data);
+        dd($this->pie);
     }
 
     public function render()
--- resources/views/livewire/dashboard/causes-analysis/grapf.blade.php
+++ resources/views/livewire/dashboard/causes-analysis/grapf.blade.php
@@ -1,9 +1,56 @@
 <div>
+     <x-input-daterange id="rangeDate-pie" placeholder='date-range' />
     <div id="chart-pie"style="height: 400px;"></div>
 </div>
 @push('scripts')
+ <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 <script src="https://echarts.apache.org/en/js/vendors/echarts/dist/echarts.min.js"></script>
 <script>
+     // Date range
+       flatpickr("#rangeDate-pie", {
+           mode: 'range'
+           , dateFormat: "d-m-Y", //defaults to "F Y"
+           onChange: function(dates) {
+               if (dates.length === 2) {
+
+                   var start = new Date(dates[0]);
+                   var end = new Date(dates[1]);
+
+                   var year = start.getFullYear();
+                   var month = start.getMonth() + 1;
+                   var dt = start.getDate();
+
+                   if (dt < 10) {
+                       dt = '0' + dt;
+                   }
+                   if (month < 10) {
+                       month = '0' + month;
+                   }
+                   var year2 = end.getFullYear();
+                   var month2 = end.getMonth() + 1;
+                   var dt2 = end.getDate();
+
+                   if (dt2 < 10) {
+                       dt2 = '0' + dt2;
+                   }
+                   if (month2 < 10) {
+                       month2 = '0' + month2;
+                   }
+
+                   // var tglMulai = year + '-' + month + '-' + dt;
+                   // var tglAkhir = year2 + '-' + month2 + '-' + dt2;
+
+                   var tglMulai = year + '-' + month + '-' + dt;
+                   var tglAkhir = year2 + '-' + month2 + '-' + dt2;
+                   @this.set('tglMulai', tglMulai)
+                   @this.set('tglAkhir', tglAkhir)
+               } else {
+                   @this.set('tglMulai', null)
+                   @this.set('tglAkhir', null)
+               }
+           }
+       });
+       // end date range
     var dom = document.getElementById('chart-pie');
     var myChart = echarts.init(dom, null, {
         renderer: 'canvas'
