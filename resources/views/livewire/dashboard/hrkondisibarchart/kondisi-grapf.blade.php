ebe7dbc456df0358015c5701883691f58e3720a4
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-14 13:53:22 +0800

    sds

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -1,47 +1,41 @@
    <div>
-       <div id="chartKondisi" style="height: 400px;"></div>
+       <div id="chart-container" style="height: 400px;"></div>
    </div>
 
    @push('scripts')
    <script src="https://echarts.apache.org/en/js/vendors/echarts/dist/echarts.min.js"></script>
    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
    <script>
-       // Date range
-       document.addEventListener('livewire:navigated', function() {
-           let chartDom = document.getElementById('chartKondisi');
-           let myChart = echarts.init(chartDom);
-
-           let option = {
-               title: {
-                   text: 'Laporan Hazard per Kondisi'
-               }
-               , tooltip: {}
-               , xAxis: {
-                   type: 'category'
-                   , data: []
-               }
-               , yAxis: {
-                   type: 'value'
-               }
-               , series: [{
-                   type: 'bar'
-                   , data: []
-               }]
-           };
-
-           myChart.setOption(option);
-
-           // Event listener di Livewire 3
-           Livewire.on('updateChart', (payload) => {
-               myChart.setOption({
-                   xAxis: {
-                       data: payload.data.map(v => v.label)
-                   }
-                   , series: [{
-                       data: payload.data.map(v => v.count)
-                   }]
-               });
-           });
+       var dom = document.getElementById('chart-container');
+       var myChart = echarts.init(dom, null, {
+           renderer: 'canvas'
+           , useDirtyRect: false
        });
+       var app = {};
+
+
+       var option;
+
+       option = {
+           xAxis: {
+               type: 'category'
+               , data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+           }
+           , yAxis: {
+               type: 'value'
+           }
+           , series: [{
+               data: [120, 200, 150, 80, 70, 110, 130]
+               , type: 'bar'
+           }]
+       };
+
+
+       if (option && typeof option === 'object') {
+           myChart.setOption(option);
+       }
+
+       window.addEventListener('resize', myChart.resize);
+
    </script>
    @endpush
