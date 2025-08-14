16c1d66682a75593b2cd753bab22b07d8b27d91d
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-14 11:40:01 +0800

    sa

--- app/Livewire/Dashboard/Hrkondisibarchart/KondisiGrapf.php
+++ app/Livewire/Dashboard/Hrkondisibarchart/KondisiGrapf.php
@@ -24,7 +24,7 @@ class KondisiGrapf extends Component
     }
     public function mount()
     {
-        $user = Auth::user()->id;
+        $user = auth()->user();
         $query = HazardReport::select('kondisitidakamen_id', DB::raw('COUNT(*) as total'))
             ->whereNotNull('kondisitidakamen_id')
             ->with('kondisiTidakAman');
@@ -55,7 +55,7 @@ class KondisiGrapf extends Component
     }
     public function loadChartData()
     {
-        $user = Auth::user()->id;
+        $user = auth()->user();
 
         $query = HazardReport::select('kondisitidakamen_id', DB::raw('COUNT(*) as total'))
             ->whereNotNull('kondisitidakamen_id')
