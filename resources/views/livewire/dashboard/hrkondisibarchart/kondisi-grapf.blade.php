a2330b0c6a6e8b20b887a8c2880ec2642b4fd98d
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-15 08:18:22 +0800

    saasssa

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -1,4 +1,5 @@
    <div>
+    {{ $tglMulai    $tglAkhir }}
        <x-input-daterange id="tanggal_range" placeholder='date-range' />
        <div wire:ignore id="chart-container" style="height: 400px;"></div>
    <script src="https://echarts.apache.org/en/js/vendors/echarts/dist/echarts.min.js"></script>
