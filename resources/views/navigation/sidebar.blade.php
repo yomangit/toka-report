8a5fb31ad789c9e6b442d36f755a5cd45947ece5
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-15 16:10:02 +0800

    ddsd

--- resources/views/navigation/sidebar.blade.php
+++ resources/views/navigation/sidebar.blade.php
@@ -12,7 +12,7 @@
     <ul class="mt-12 menu menu-xs">
         <li>
             <details {{ Request::is('dashboard/*') ? ' open' : '' }}>
-                <summary class="{{ Request::is('/*') ? ' text-primary-muted font-semibold' : '' }}">
+                <summary class="{{ Request::is('dashboard/*') ? ' text-primary-muted font-semibold' : '' }}">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
                         <path d="M8.543 2.232a.75.75 0 0 0-1.085 0l-5.25 5.5A.75.75 0 0 0 2.75 9H4v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 1 1 2 0v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V9h1.25a.75.75 0 0 0 .543-1.268l-5.25-5.5Z" />
                     </svg>
--- routes/web.php
+++ routes/web.php
@@ -87,7 +87,7 @@ Route::get('eventReport/hazardReportGuest/{workflow_template_id?}', HazardReport
 Route::get('manhours/manhoursTable', WebAccess::class)->name('WebAccess');
 Route::get('eventReport/hazardReportExcel', TableExcel::class)->name('TableExcel');
 Route::middleware(['auth', 'auth.session'])->group(function () {
-
+    Route::redirect('/', 'dashboard/hazard');
     Route::get('dashboard/incident', dashoard::class)->name('dashboard-incident');
     Route::get('dashboard/hazard', HrGrapf::class)->name('dashboard-hazard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
