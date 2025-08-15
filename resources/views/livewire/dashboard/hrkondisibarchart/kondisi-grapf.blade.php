5fd7ab4136451aa00fe1d8739b76d5cc6eea4eb2
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-15 15:13:49 +0800

    dd

--- resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
+++ resources/views/livewire/dashboard/hrkondisibarchart/kondisi-grapf.blade.php
@@ -1,12 +1,12 @@
    <div>
        <x-input-daterange id="tanggal_range" placeholder='date-range' />
        <div class="grid gap-4 md:grid-cols-2">
-           <div class="card card-border bg-base-100 w-96">
+           <div class="card card-border bg-base-300 ">
                <div class="card-body">
                    <div wire:ignore id="chart_kondisi" style="height: 400px;"></div>
                </div>
            </div>
-           <div class="card card-border bg-base-100 w-96">
+           <div class="card card-border bg-base-300 ">
                <div class="card-body">
                    <div wire:ignore id="chart_tindakan" style="height: 400px;"></div>
                </div>
