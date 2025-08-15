3d498482d0c56e25e8724015532fd34cc0c36d09
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-15 15:44:30 +0800

    s

--- resources/views/livewire/dashboard/hazard-grap/hr-grapf.blade.php
+++ resources/views/livewire/dashboard/hazard-grap/hr-grapf.blade.php
@@ -1,10 +1,4 @@
 <div>
-
-    @if (auth()->user()->role_user_permit_id == 1 || auth()->user()->can_view_own_division)
-    <div id="divisionChart" data-labels='@json($divisionLabels)' data-counts='@json($divisionCounts)' data-colors='@json($divisionColors)'></div>
-    @endif
-
-   {{-- <div><livewire:dashboard.causes-analysis.grapf></div> --}}
     <livewire:dashboard.hrkondisibarchart.kondisi-grapf>
 </div>
 
