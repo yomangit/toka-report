091fba3607eabbbeaeaa4ad33741f6873bef2e72
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-15 15:54:34 +0800

    sasa

--- resources/views/navigation/sidebar.blade.php
+++ resources/views/navigation/sidebar.blade.php
@@ -11,13 +11,21 @@
 
     <ul class="mt-12 menu menu-xs">
         <li>
+            <details {{ Request::is('/*') ? ' open' : '' }}>
+                <summary class="{{ Request::is('eventReport*') ? ' text-primary-muted font-semibold' : '' }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                        <path d="M8.543 2.232a.75.75 0 0 0-1.085 0l-5.25 5.5A.75.75 0 0 0 2.75 9H4v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 1 1 2 0v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V9h1.25a.75.75 0 0 0 .543-1.268l-5.25-5.5Z" />
+                    </svg>
 
-
-            <a href="{{ route('dashboard') }}" class="{{ Request::is('/') ? 'text-primary-muted font-semibold' : '' }}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
-                    <path d="M8.543 2.232a.75.75 0 0 0-1.085 0l-5.25 5.5A.75.75 0 0 0 2.75 9H4v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 1 1 2 0v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V9h1.25a.75.75 0 0 0 .543-1.268l-5.25-5.5Z" />
-                </svg>
-                Dashboard</a>
+                   Dashboard
+                </summary>
+                <ul class="p-1 w-52 bg-base-400 menu menu-xs">
+                    <li><a href="{{ route('dashboard-hazard-report') }}" class="{{ Request::is('/hazard/dashboard') ? 'active text-emerald-500 font-semibold' : '' }}">{{ __('Incident Report') }}</a>
+                    </li>
+                    <li><a href="{{ route('dashboard') }}" class="{{ Request::is('/') ? 'active text-emerald-500 font-semibold' : '' }}">{{ __('Hazard Report') }}</a>
+                    </li>
+                </ul>
+            </details>
         </li>
         @auth
         @if (auth()->user()->hasRolePermit('administration', 'auth'))
