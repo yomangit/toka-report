8d26e0f2da59ff2c820ec2cf455004692f0a22a2
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-18 07:56:36 +0800

    sss

--- resources/views/livewire/dashboard/main-dashboard.blade.php
+++ resources/views/livewire/dashboard/main-dashboard.blade.php
@@ -1,15 +1,1 @@
-<div>
 
-    <div class="tabs tabs-lift">
-        <input type="radio" name="my_tabs_3" class="tab" aria-label="Hazard" checked="checked"/>
-        <div class="p-6 tab-content bg-base-100 border-base-300">
-            <livewire:dashboard.hazard-grap.hr-grapf>
-        </div>
-
-        <input type="radio" name="my_tabs_3" class="tab" aria-label="Incident"  />
-        <div class="p-6 tab-content bg-base-100 border-base-300">
-            <livewire:dashboard.index>
-        </div>
-
-    </div>
-</div>
