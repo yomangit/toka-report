c6e5efcb1fc8dc2dcf689904267da878b6fb52b1
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-18 08:02:47 +0800

    sd

--- resources/views/livewire/dashboard/hazard-grap/hr-grapf.blade.php
+++ resources/views/livewire/dashboard/hazard-grap/hr-grapf.blade.php
@@ -1,4 +1,15 @@
 <div>
-    <livewire:dashboard.hrkondisibarchart.kondisi-grapf>
-</div>
+    <div role="tablist" class="tabs tabs-lifted">
+        <input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Hazard" checked="checked" />
+        <div role="tabpanel" class="p-6 tab-content bg-base-100 border-base-300 rounded-box">
+            <livewire:dashboard.hrkondisibarchart.kondisi-grapf>
+        </div>
 
+        <input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Tab 3" />
+        <div role="tabpanel" class="p-6 tab-content bg-base-100 border-base-300 rounded-box">
+            Tab content 3
+        </div>
+    </div>
+
+   
+</div>
