02cbc864964ef8f214b7ef9a265398a3065fd040
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-18 11:29:42 +0800

    dsd

--- resources/views/livewire/event-report/hazard-report-guest/create.blade.php
+++ resources/views/livewire/event-report/hazard-report-guest/create.blade.php
@@ -74,7 +74,10 @@
                 <div class="w-full max-w-md xl:max-w-xl form-control">
                     <x-label-req :value="__('Perusahaan terkait')" />
                     <div class="dropdown dropdown-end">
-                        <x-input wire:click='clickWorkgroup' wire:model.live='workgroup_name' wire:keydown.self="changeConditionDivision" placeholder='cari divisi...' :error="$errors->get('workgroup_name')" class="cursor-pointer" tabindex="0" role="button" />
+                        <div class="relative">
+                            <x-input wire:click='clickWorkgroup' wire:model.live='workgroup_name' wire:keydown.self="changeConditionDivision" placeholder='cari divisi...' :error="$errors->get('workgroup_name')" class="cursor-pointer" tabindex="0" role="button" />
+                            <span wire:target="division_id,workgroup_name" wire:loading.class="absolute right-0 transform -translate-y-1/2 top-1/2 loading loading-spinner text-warning"></span>
+                        </div>
                         <div tabindex="0" class="z-10 w-full   overflow-y-auto shadow dropdown-content card card-compact bg-base-200 text-primary-content {{ $hiddenWorkgroup }}">
                             <ul class="h-full px-4 py-4 list-disc list-inside max-h-40 bg-base-200 rounded-box">
                                 @forelse ($Division as $item)
