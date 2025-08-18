1be327ca432a005bec575e8d5015b1e4d19c685f
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-18 08:13:29 +0800

    dsd

--- resources/views/navigation/sidebar.blade.php
+++ resources/views/navigation/sidebar.blade.php
@@ -11,7 +11,7 @@
 
     <ul class="mt-12 menu menu-xs">
         <li>
-            <a href="{{ route('dashboard') }}" class="{{ Request::is('/') ? 'active text-emerald-500 font-semibold' : '' }}">
+            <a href="{{ route('/') }}" class="{{ Request::is('/') ? 'active text-emerald-500 font-semibold' : '' }}">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
                     <path d="M8.543 2.232a.75.75 0 0 0-1.085 0l-5.25 5.5A.75.75 0 0 0 2.75 9H4v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 1 1 2 0v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V9h1.25a.75.75 0 0 0 .543-1.268l-5.25-5.5Z" />
                 </svg>
