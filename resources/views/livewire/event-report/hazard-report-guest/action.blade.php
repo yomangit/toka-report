1fd9dea8ff5957c8de0a9e89d6510b2611e914a7
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-18 09:16:41 +0800

    sas

--- resources/views/livewire/event-report/hazard-report-guest/action.blade.php
+++ resources/views/livewire/event-report/hazard-report-guest/action.blade.php
@@ -49,6 +49,11 @@
                     <x-label-error :messages="$errors->get('responsibility_name')" />
                 </div>
                 <div class="w-full max-w-xs sm:max-w-sm xl:max-w-xl form-control">
+                    <x-label-no-req :value="__('Tanggal Penemuan Hazard')" />
+                    <x-input-date  wire:model.live='orginal_date' readonly :error="$errors->get('due_date')" />
+                    <x-label-error :messages="$errors->get('due_date')" />
+                </div>
+                <div class="w-full max-w-xs sm:max-w-sm xl:max-w-xl form-control">
                     <x-label-no-req :value="__('Tenggat Waktu')" />
                     <x-input-date id="due_date_guest" wire:model.live='due_date' readonly :error="$errors->get('due_date')" />
                     <x-label-error :messages="$errors->get('due_date')" />
