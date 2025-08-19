13d78c57a62ec19b5a31f8920d002498c038434a
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-08-19 11:34:11 +0800

    sdf

--- app/Livewire/EventReport/HazardReportGuest/Create.php
+++ app/Livewire/EventReport/HazardReportGuest/Create.php
@@ -526,4 +526,21 @@ class Create extends Component
         $this->dispatch('refreshChartHazard');
         // $this->redirectRoute('hazardReportCreate', ['workflow_template_id' => $this->workflow_template_id]);
     }
+     public function clearFields()
+    {
+        $this->report_byName               = "";
+        $this->report_toName               = "";
+        $this->workgroup_name              = "";
+        $this->division_id                 = "";
+        $this->date                        = "";
+        $this->documentation               = "";
+        $this->description                 = "";
+        $this->immediate_corrective_action = "";
+        $this->location_name               = "";
+        $this->location_id                 = "";
+        $this->kondisi_tidak_aman          = "";
+        $this->tindakan_tidak_aman         = "";
+        $this->tindakkan_selanjutnya         = "";
+        $this->workgroup_name              = "";
+    }
 }
