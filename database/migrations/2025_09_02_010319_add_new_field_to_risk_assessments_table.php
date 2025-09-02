f9fa79db3a575c4ec4849002ff6e19903a132bef
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-02 09:03:41 +0800

    dsd

---
+++ database/migrations/2025_09_02_010319_add_new_field_to_risk_assessments_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('risk_consequences', function (Blueprint $table) {
+             $table->string('level')->nullable(); // contoh: 1, 2, 3, 4, 5
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('risk_consequences', function (Blueprint $table) {
+             $table->string('level')->nullable(); // contoh: 1, 2, 3, 4, 5
+        });
+    }
+};
