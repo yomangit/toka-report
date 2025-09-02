49d8f5d5792515f6576e33ceb28fb126df81b5d0
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-02 11:24:29 +0800

    sas

--- database/migrations/2025_09_02_025819_create_risk_assessment_matrix_table.php
+++ database/migrations/2025_09_02_025819_create_risk_assessment_matrix_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('risk_assessment_matrix', function (Blueprint $table) {
             $table->id();
-              $table->id();
             $table->foreignId('risk_assessment_id')->constrained('risk_assessments')->cascadeOnDelete();
             $table->foreignId('risk_matrix_cell_id')->constrained('risk_matrix_cells')->cascadeOnDelete();
             $table->timestamps();
