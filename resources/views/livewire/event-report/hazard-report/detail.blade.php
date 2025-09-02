b8f11f6da34cb644d49156fdbd6a9e90c3cbd504
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-02 12:21:14 +0800

    sdf

--- resources/views/livewire/event-report/hazard-report/detail.blade.php
+++ resources/views/livewire/event-report/hazard-report/detail.blade.php
@@ -345,7 +345,7 @@
                                             @endphp
                                             <td class="border cursor-pointer w-4 {{ $color }}
                                     @if($risk_likelihood_id == $l->id && $risk_consequence_id == $c->id) border-2 border-stone-500 @endif" wire:click="riskId({{ $l->id }}, {{ $c->id }})">
-                                                <div class="text-[6px]">{{ Str::upper(substr($severity, 0, 1)) }}</div>
+                                                <div class="text-xs font-semibold">{{ Str::upper(substr($severity, 0, 1)) }}</div>
 
                                             </td>
 
