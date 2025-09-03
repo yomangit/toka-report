6d39ac0b028a39ee095cb98a08adc6677ab01923
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-03 10:07:09 +0800

    dsd

--- resources/views/livewire/event-report/hazard-report/table-excel.blade.php
+++ resources/views/livewire/event-report/hazard-report/table-excel.blade.php
@@ -29,7 +29,7 @@
             <tr class="text-center">
 
                 <td>{{ $hr->reference }}</td>
-                <td>{{ DateTime::createFromFormat('Y-m-d : H:i', $hr->date)->format('m-d-Y : H:i') }}</td>
+                <td>{{$hr->date}}</td>
                 <td> {{ $hr->report_byName }}</td>
                 <td>{{$hr->eventType->type_eventreport_name}}</td>
                 <td>{{ $hr->subEventType->event_sub_type_name }}</td>
