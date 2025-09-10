c2441a6bf7b3a34b0388040297c73c916576d67b
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-11 07:48:55 +0800

    saa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="username" :value="__('Username')" />
-            <x-text-input id="username" class="block w-full mt-1" type="text" name="username" :value="old('username')" :error="$errors->get('username')"  autofocus autocomplete="username" />
+            <x-text-input id="username" class="block w-full mt-1" type="text" name="username" :value="old('username')" :error="$errors->get('username')" autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
@@ -16,11 +16,7 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block w-full mt-1"
-                            type="password"
-                            :error="$errors->get('password')"
-                            name="password"
-                             autocomplete="current-password" />
+            <x-text-input id="password" class="block w-full mt-1" type="password" :error="$errors->get('password')" name="password" autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -35,15 +31,19 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
+            <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                {{ __('Forgot your password?') }}
+            </a>
             @endif
-            
+            @php
+            // contoh logika: kalau ada user yang belum punya password
+            $allowRegister = \App\Models\User::whereNull('password')->exists();
+            @endphp
+            @if ($allowRegister)
             <a class="text-sm text-gray-600 underline rounded-md ms-3 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
                 {{ __('Register?') }}
             </a>
-            
+            @endif
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
