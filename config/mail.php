f7e20fd5df17baa597a211d85382ecd44f76bcd8
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-18 16:24:27 +0800

    asasas

--- config/mail.php
+++ config/mail.php
@@ -38,16 +38,16 @@ return [
     'mailers' => [
 
         'smtp' => [
-            'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.office365.com'),
-            'port' => env('MAIL_PORT', 587),
+            'transport' => 'mail',
+            'url' => env('MAIL_URL'),
+            'host' => env('MAIL_HOST', '127.0.0.1'),
+            'port' => env('MAIL_PORT', 2525),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
-
 
         'ses' => [
             'transport' => 'ses',
