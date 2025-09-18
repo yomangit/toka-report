ac5752d8ab3df1ce7c24723a6199b0e010f40dae
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-18 16:15:28 +0800

    sasas

--- config/mail.php
+++ config/mail.php
@@ -45,7 +45,7 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
 
