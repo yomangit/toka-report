a652c2942c46da24ff04e882a426a0449cbf9d50
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-23 10:33:57 +0800

    sgf

--- resources/views/livewire/event-report/hazard-report/create-and-update.blade.php
+++ resources/views/livewire/event-report/hazard-report/create-and-update.blade.php
@@ -48,7 +48,7 @@
                         <x-label-req :value="__('Dilaporkan Oleh')" />
                         <div class="relative">
                             <!-- Input Search -->
-                            <input name="searchPelapor" type="text" wire:model.live="searchPelapor" placeholder="Cari Nama Pelapor..." class="input input-bordered w-full max-w-sm focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs {{ $errors->has('pelapor_id') ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '' }}"  />
+                            <input name="searchPelapor" type="text" wire:model.live="searchPelapor" placeholder="Cari Nama Pelapor..." class="input input-bordered w-full max-w-sm focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs {{ $errors->has('pelapor_id') ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '' }}"x-ref="searchInput"  />
                             <!-- Dropdown hasil search (teleport keluar collapse) -->
                             @if ($showPelaporDropdown)
                             <template wire:ignore x-teleport="body">
