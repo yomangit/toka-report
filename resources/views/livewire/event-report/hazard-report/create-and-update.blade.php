64141f0ff9ca8c443563fff122006030249d169f
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-23 13:17:42 +0800

    ssasa

--- resources/views/livewire/event-report/hazard-report/create-and-update.blade.php
+++ resources/views/livewire/event-report/hazard-report/create-and-update.blade.php
@@ -165,7 +165,6 @@
                         <div class="relative">
                             <!-- Input Search -->
                             <x-input wire:model.live='searchLocation' placeholder='cari divisi...' :error="$errors->get('location_id')" class="cursor-pointer" />
-                            <input type="text" wire:model.live.debounce.300ms="searchLocation" placeholder="Cari Lokasi..." class="input input-bordered w-full focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs {{ $errors->has('location_id') ? 'ring-1 ring-rose-500 focus:ring-rose-500 focus:border-rose-500' : '' }}" />
                             <!-- Dropdown hasil search -->
                             @if($showLocationDropdown && count($locations) > 0)
                             <ul class="absolute z-10 w-full mt-1 overflow-auto border rounded-md shadow bg-base-100 max-h-60">
