dffb8d90309078ffd3451c4491c903c8df815f64
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-10-24 09:13:45 +0800

    asdfa

--- app/Livewire/EventReport/HazardReport/CreateAndUpdate.php
+++ app/Livewire/EventReport/HazardReport/CreateAndUpdate.php
@@ -518,6 +518,7 @@ class CreateAndUpdate extends Component
             'actual_close_date' => $this->actual_close_date,
             'responsible_id' => $this->action_responsible_id,
         ];
+        dd($this->actions);
         $this->dispatch('alert', [
             'text' => "Tindakan Lanjutan berhasil dibuat!!",
             'duration' => 5000,
