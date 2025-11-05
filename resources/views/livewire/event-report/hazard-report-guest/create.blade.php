9d3dc883faf300be68fcc42b71014ba1e26d7806
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-11-05 14:52:44 +0800

    ssa

--- resources/views/livewire/event-report/hazard-report-guest/create.blade.php
+++ resources/views/livewire/event-report/hazard-report-guest/create.blade.php
@@ -132,6 +132,8 @@
                         @endif
                     </fieldset>
                 </div>
+                 <x-label-error :messages="$errors->get('manualPelaporName')" />
+                    <x-label-error :messages="$errors->get('site_id')" />
                 @if ($pilihDivisiPelapor)
                 <div class="w-full max-w-md xl:max-w-xl form-control">
                     <fieldset>
