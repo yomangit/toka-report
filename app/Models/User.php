71db2e685be05849bbfee7515e86c7aa5e353b72
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-11-07 22:13:46 +0800

    sasa

--- app/Models/User.php
+++ app/Models/User.php
@@ -140,14 +140,15 @@ class User extends Authenticatable implements LdapAuthenticatable
     {
         return $this->belongsToMany(ResponsibleRole::class, 'event_user_securities');
     }
-    public function Responsible()
+    public function getResponsibleRoleName($id)
     {
-        return $this->belongsTo(EventUserSecurity::class,'user_id');
+        $role = $this->ResponsibleRole->firstWhere('id', $id);
+        return $role?->pivot->name;
     }
     public function hasResponsibleRoleId($id)
-{
-    return $this->ResponsibleRole->contains('id', $id);
-}
+    {
+        return $this->ResponsibleRole->contains('id', $id);
+    }
     public function moderatorAkases()
     {
         return $this->belongsToMany(ResponsibleRole::class, 'event_user_securities')
