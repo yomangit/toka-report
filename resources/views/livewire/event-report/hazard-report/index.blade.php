ccbde5b7d440a87e1a158f6c8db4050552c8a6d5
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-11-07 22:14:56 +0800

    sds

--- resources/views/livewire/event-report/hazard-report/index.blade.php
+++ resources/views/livewire/event-report/hazard-report/index.blade.php
@@ -93,7 +93,7 @@
                     <td>
                         <div>
                             @if (
-                            $item->workgroup_name== auth()->user()->getResponsibleRoleName(1) ||
+                            $item->workgroup_name === auth()->user()->getResponsibleRoleName(1) ||
                             auth()->user()->role_user_permit_id == 1 ||
                             $item->submitter == auth()->id() ||
                             $item->report_by == auth()->id() ||
