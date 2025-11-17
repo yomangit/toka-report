85842aaff393d50b49d4075d8c97a6afd8266bb4
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-11-17 08:42:11 +0800

    sasa

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,7 +76,7 @@ class User extends Authenticatable implements LdapAuthenticatable
     }
     public function company()
     {
-        return $this->belongsTo(Company::class);
+        return $this->belongsTo(Company::class,'company_id');
     }
     public function Dept()
     {
